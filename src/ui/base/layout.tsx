a1a2cd13b6bb009ee0deb992e09e90e5a5ad3d85
RaenonX <raenonx0710@gmail.com>
2023-07-24 15:46:08 -0500

    Fixed discontinued page background

--- src/ui/base/layout.tsx
+++ src/ui/base/layout.tsx
@@ -7,7 +7,7 @@ import {classNames} from '@/utils/react';
 export const PageLayout = ({children}: React.PropsWithChildren) => {
   return (
     <main className={classNames(
-      'flex w-full flex-col gap-1.5 bg-gradient-radial p-2 transform-gpu transition-colors',
+      'flex min-h-full w-full flex-col gap-1.5 bg-gradient-radial p-2 transform-gpu transition-colors',
       'from-slate-200 to-slate-300 text-neutral-900 dark:from-slate-800 dark:to-slate-950 dark:text-neutral-100',
     )}>
       <NavBar/>
