cfcea100446dbdcb3f1191342230c4192a0cb251
RaenonX <raenonx0710@gmail.com>
2023-07-25 01:13:19 -0500

    Minor style/issue fix

--- src/components/announcement/styles.ts
+++ src/components/announcement/styles.ts
@@ -2,10 +2,10 @@ import {AnnouncementLevel} from '@/types/mongo/announcement';
 
 
 export const alertClasses: {[level in AnnouncementLevel]: string} = {
-  info: 'text-cyan-600 dark:text-cyan-300',
-  ok: 'text-green-600 dark:text-green-300',
-  warning: 'text-amber-600 dark:text-amber-300',
-  alert: 'text-red-600 dark:text-red-300',
+  info: 'text-blue-700 dark:text-cyan-300',
+  ok: 'text-emerald-700 dark:text-green-300',
+  warning: 'text-yellow-700 dark:text-amber-300',
+  alert: 'text-red-700 dark:text-red-300',
 };
 
 export const alertClassCommon = 'h-10 overflow-hidden text-sm md:h-5';
