9d60dfdf30abec9d4d1a8af3c8c0f37169d5afca
RaenonX <raenonx0710@gmail.com>
2023-07-28 01:03:37 -0500

    Added level slider in cooking page

--- src/ui/cooking/input/recipeSingle.tsx
+++ src/ui/cooking/input/recipeSingle.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import {useTranslations} from 'next-intl';
 
 import {InputBox} from '@/components/input/box';
+import {Slider} from '@/components/input/slider';
 import {Flex} from '@/components/layout/flex';
 import {Meal} from '@/types/mongo/meal';
 import {CookingRecipeLayout} from '@/ui/cooking/recipeLayout';
@@ -14,30 +15,47 @@ type Props = Omit<CookingCommonProps, 'meals'> & {
 };
 
 export const CookingInputRecipeSingle = ({input, setInput, data}: Props) => {
-  const {id} = data;
+  const {id, levels} = data;
   const t = useTranslations('UI.InPage.Cooking');
 
+  const recipeLevel = input.recipeLevel[id] ?? 1;
+
+  const setLevel = (level: number) => {
+    setInput((original) => ({
+      ...original,
+      recipeLevel: {
+        ...original.recipeLevel,
+        [id]: level || 1,
+      },
+    }));
+  };
+
   return (
-    <CookingRecipeLayout mealId={id} imageSizeClass="h-16 w-16">
+    <CookingRecipeLayout mealId={id} imageSizeClass="h-20 w-20">
       <Flex direction="row" className="items-center gap-1.5">
-        <div className="text-xs text-slate-500 dark:text-slate-400">
-          {t('RecipeLevel')}
+        <Flex direction="col" className="w-14" noFullWidth>
+          <div className="whitespace-nowrap text-xs text-slate-500 dark:text-slate-400">
+            {t('RecipeLevel')}
+          </div>
+          <InputBox
+            id={`capacity-${id}`}
+            type="number"
+            step="1"
+            min="1"
+            className="text-center text-sm"
+            value={recipeLevel}
+            onChange={({target}) => setLevel(Number(target.value))}
+          />
+        </Flex>
+        <div className="w-full self-end">
+          <Slider
+            id={`capacity-${id}-slider`}
+            min="1"
+            max={levels.length}
+            value={recipeLevel}
+            setValue={(newValue) => setLevel(newValue)}
+          />
         </div>
-        <InputBox
-          id="capacity"
-          type="number"
-          step="1"
-          min="1"
-          className="w-10 text-center"
-          value={input.recipeLevel[id] ?? 1}
-          onChange={(e) => setInput((original) => ({
-            ...original,
-            recipeLevel: {
-              ...original.recipeLevel,
-              [id]: Number(e.target.value) || 1,
-            },
-          }))}
-        />
       </Flex>
     </CookingRecipeLayout>
   );
