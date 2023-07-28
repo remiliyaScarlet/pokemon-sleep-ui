6db511654bd1a4f16c5eb0e705a14650ee66a771
RaenonX <raenonx0710@gmail.com>
2023-07-28 05:10:35 -0500

    Added ingredient link in ingredient icons

--- src/components/shared/food/ingredientIcons.tsx
+++ src/components/shared/food/ingredientIcons.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
 import Image from 'next/image';
+import Link from 'next/link';
 import {useTranslations} from 'next-intl';
 
+import {Flex} from '@/components/layout/flex';
 import {textShadow} from '@/styles/classes';
 import {imageIconSizes} from '@/styles/image';
 import {Meal} from '@/types/mongo/meal';
@@ -20,14 +22,16 @@ export const IngredientIcons = ({meal, useTextShadow = true}: Props) => {
   return (
     <>
       {meal.ingredients.map(({id, quantity}) => (
-        <>
-          <div className="relative h-4 w-4">
-            <Image src={`/images/ingredient/${id}.png`} alt={t(id.toString())} fill sizes={imageIconSizes}/>
-          </div>
-          <div className={classNames('text-xs', useTextShadow ? textShadow : undefined)}>
-            {quantity}
-          </div>
-        </>
+        <Link key={id} href={`/ingredient/${id}`}>
+          <Flex direction="row" className="gap-0.5">
+            <div className="relative h-4 w-4">
+              <Image src={`/images/ingredient/${id}.png`} alt={t(id.toString())} fill sizes={imageIconSizes}/>
+            </div>
+            <div className={classNames('text-xs', useTextShadow ? textShadow : undefined)}>
+              {quantity}
+            </div>
+          </Flex>
+        </Link>
       ))}
     </>
   );
