5adba35104c110dc07c67853fa23139bbc1cc5ca
RaenonX <raenonx0710@gmail.com>
2023-07-30 04:39:56 -0500

    Refactored nav entries

--- src/const.ts
+++ src/const.ts
@@ -1,3 +1,6 @@
+import {NavEntry} from '@/types/nav';
+
+
 export const localeName = {
   en: 'English',
   zh: '中文',
@@ -6,3 +9,38 @@ export const localeName = {
 } as const;
 
 export const locales = Object.keys(localeName);
+
+export const navEntries: NavEntry[] = [
+  {
+    href: '/pokedex',
+    imageSrc: '/images/generic/pokeball.png',
+    i18nTextId: 'Pokedex.Index.Title',
+  },
+  {
+    href: '/cooking',
+    imageSrc: '/images/generic/pot.png',
+    i18nTextId: 'Cooking.Title',
+  },
+  {
+    href: '/meal',
+    imageSrc: '/images/generic/meal.png',
+    i18nTextId: 'Meal.Index.Title',
+  },
+  {
+    href: '/ingredient',
+    imageSrc: '/images/generic/ingredient.png',
+    i18nTextId: 'Ingredient.Index.Title',
+  },
+  {
+    href: '/map',
+    imageSrc: '/images/generic/map.png',
+    i18nTextId: 'Map.Index.Title',
+    disabled: true,
+  },
+  {
+    href: '/skill',
+    imageSrc: '/images/generic/skill.png',
+    i18nTextId: 'Skill.Index.Title',
+    disabled: true,
+  },
+];
--- src/hooks/nav.ts
+++ src/hooks/nav.ts
@@ -1,43 +1,8 @@
 'use client';
 import React from 'react';
 
-import {NavEntry} from '@/types/nav';
+import {navEntries} from '@/const';
 
-
-const navEntries: NavEntry[] = [
-  {
-    href: '/pokedex',
-    imageSrc: '/images/generic/pokeball.png',
-    i18nTextId: 'Pokedex.Index.Title',
-  },
-  {
-    href: '/cooking',
-    imageSrc: '/images/generic/pot.png',
-    i18nTextId: 'Cooking.Title',
-  },
-  {
-    href: '/meal',
-    imageSrc: '/images/generic/meal.png',
-    i18nTextId: 'Meal.Index.Title',
-  },
-  {
-    href: '/ingredient',
-    imageSrc: '/images/generic/ingredient.png',
-    i18nTextId: 'Ingredient.Index.Title',
-  },
-  {
-    href: '/map',
-    imageSrc: '/images/generic/map.png',
-    i18nTextId: 'Map.Index.Title',
-    disabled: true,
-  },
-  {
-    href: '/skill',
-    imageSrc: '/images/generic/skill.png',
-    i18nTextId: 'Skill.Index.Title',
-    disabled: true,
-  },
-];
 
 // Need to "cache" the nav entries (effectively making whatever using nav entries a client component)
 // If this is not stored, fast refresh will not work i.e. every page change triggers full reload
