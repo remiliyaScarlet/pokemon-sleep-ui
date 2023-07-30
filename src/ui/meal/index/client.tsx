0cf1a99ced3b174fb44be6addc58bd500eb0bb20
RaenonX <raenonx0710@gmail.com>
2023-07-30 03:04:47 -0500

    Added meal index recipe sorting #15
    closes #15

--- src/ui/meal/index/client.tsx
+++ src/ui/meal/index/client.tsx
@@ -7,6 +7,7 @@ import {Meal} from '@/types/mongo/meal';
 import {useFilteredMeals} from '@/ui/meal/index/hooks';
 import {MealInput} from '@/ui/meal/index/input/main';
 import {MealLink} from '@/ui/meal/index/link';
+import {toSum} from '@/utils/array';
 import {classNames} from '@/utils/react';
 
 
@@ -23,19 +24,41 @@ export const MealIndexClient = ({data}: Props) => {
       <MealInput data={data} {...props}/>
       <HorizontalSplitter/>
       <Flex direction="row" wrap className="gap-1.5 md:p-3 lg:p-5">
-        {data.map((meal) => (
-          <div
-            key={meal.id}
-            className={classNames(
-              'relative width-with-gap',
-              'sm:width-with-gap-2-items md:width-with-gap-3-items',
-              'lg:width-with-gap-4-items xl:width-with-gap-5-items',
-              isIncluded[meal.id] ? undefined : 'hidden',
-            )}
-          >
-            <MealLink {...meal}/>
-          </div>
-        ))}
+        {data
+          .map((meal) => ({
+            meal,
+            totalIngredientCount: toSum(meal.ingredients.map(({quantity}) => quantity)),
+          }))
+          .sort((a, b) => {
+            if (a.meal.type > b.meal.type) {
+              return 1;
+            }
+            if (a.meal.type < b.meal.type) {
+              return -1;
+            }
+
+            if (a.totalIngredientCount > b.totalIngredientCount) {
+              return -1;
+            }
+            if (a.totalIngredientCount < b.totalIngredientCount) {
+              return 1;
+            }
+
+            return 0;
+          })
+          .map(({meal}) => (
+            <div
+              key={meal.id}
+              className={classNames(
+                'relative width-with-gap',
+                'sm:width-with-gap-2-items md:width-with-gap-3-items',
+                'lg:width-with-gap-4-items xl:width-with-gap-5-items',
+                isIncluded[meal.id] ? undefined : 'hidden',
+              )}
+            >
+              <MealLink {...meal}/>
+            </div>
+          ))}
       </Flex>
     </>
   );
