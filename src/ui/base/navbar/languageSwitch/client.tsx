c10fbfc2c084679c3758a02972e3b3389000f341
RaenonX <raenonx0710@gmail.com>
2023-07-31 01:21:03 -0500

    Fixed locale switching by using `next-intl/link` #16
    Related to https://github.com/amannn/next-intl/discussions/429

--- src/ui/base/navbar/languageSwitch/client.tsx
+++ src/ui/base/navbar/languageSwitch/client.tsx
@@ -1,9 +1,9 @@
 'use client';
 import React from 'react';
 
-import {setCookie} from 'cookies-next';
 import {useLocale} from 'next-intl';
-import {usePathname, useRouter} from 'next-intl/client';
+import {usePathname} from 'next-intl/client';
+import Link from 'next-intl/link';
 
 import {Flex} from '@/components/layout/flex';
 import {Popup} from '@/components/popup';
@@ -18,13 +18,7 @@ export const LanguageSwitchClient = () => {
   const [show, setShow] = React.useState(false);
 
   const currentLocale = useLocale();
-  const router = useRouter();
   const pathname = usePathname();
-
-  const onClick = (nextLocale: string) => {
-    router.push(pathname, {locale: nextLocale});
-    setCookie('NEXT_LOCALE', nextLocale);
-  };
 
   return (
     <>
@@ -37,18 +31,12 @@ export const LanguageSwitchClient = () => {
       <Popup show={show} setShow={setShow}>
         <Flex direction="row" center wrap className="gap-2">
           {Object.entries(localeName).map(([locale, name]) => (
-            <button
-              key={locale}
-              disabled={currentLocale === locale}
-              onClick={() => onClick(locale)}
-              className={classNames(
-                'flex w-full justify-center p-5 text-xl width-with-gap sm:width-with-gap-2-items',
-                'button-base',
-                currentLocale === locale ? 'button-disabled' : 'button-clickable-border',
-              )}
-            >
+            <Link key={locale} href={pathname} locale={locale} className={classNames(
+              'button-base flex w-full justify-center p-5 text-xl width-with-gap sm:width-with-gap-2-items',
+              currentLocale === locale ? 'button-disabled' : 'button-clickable-border',
+            )}>
               {name}
-            </button>
+            </Link>
           ))}
         </Flex>
       </Popup>
