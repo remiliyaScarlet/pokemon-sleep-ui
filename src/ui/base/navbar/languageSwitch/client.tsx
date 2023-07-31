7a474a9a06006afa8aca4b31dd90497f00b63f05
RaenonX <raenonx0710@gmail.com>
2023-07-31 06:56:02 -0500

    Updated locale switch implementation

--- src/ui/base/navbar/languageSwitch/client.tsx
+++ src/ui/base/navbar/languageSwitch/client.tsx
@@ -2,8 +2,7 @@
 import React from 'react';
 
 import {useLocale} from 'next-intl';
-import {usePathname} from 'next-intl/client';
-import Link from 'next-intl/link';
+import {usePathname, useRouter} from 'next-intl/client';
 
 import {Flex} from '@/components/layout/flex';
 import {Popup} from '@/components/popup';
@@ -16,27 +15,42 @@ import styles from '../main.module.css';
 
 export const LanguageSwitchClient = () => {
   const [show, setShow] = React.useState(false);
+  const [isPending, startTransition] = React.useTransition();
 
   const currentLocale = useLocale();
+  const router = useRouter();
   const pathname = usePathname();
+
+  const onClick = (nextLocale: string) => {
+    startTransition(() => {
+      router.push(pathname, {locale: nextLocale});
+    });
+  };
 
   return (
     <>
       <button
         className={classNames('button-clickable-border text-sm px-2.5', styles['nav-button'])}
         onClick={() => setShow(true)}
+        disabled={isPending}
       >
         {isLocale(currentLocale) ? localeName[currentLocale] : `(${currentLocale})`}
       </button>
       <Popup show={show} setShow={setShow}>
         <Flex direction="row" center wrap className="gap-2">
           {Object.entries(localeName).map(([locale, name]) => (
-            <Link key={locale} href={pathname} locale={locale} className={classNames(
-              'button-base flex w-full justify-center p-5 text-xl width-with-gap sm:width-with-gap-2-items',
-              currentLocale === locale ? 'button-disabled' : 'button-clickable-border',
-            )}>
+            <button
+              key={locale}
+              disabled={isPending || currentLocale === locale}
+              onClick={() => onClick(locale)}
+              className={classNames(
+                'flex w-full justify-center p-5 text-xl width-with-gap sm:width-with-gap-2-items',
+                'button-base',
+                currentLocale === locale ? 'button-disabled' : 'button-clickable-border',
+              )}
+            >
               {name}
-            </Link>
+            </button>
           ))}
         </Flex>
       </Popup>
