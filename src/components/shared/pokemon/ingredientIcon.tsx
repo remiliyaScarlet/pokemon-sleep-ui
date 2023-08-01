c99f8ea896953a69c4dba23718e2e4c6719c5c55
RaenonX <raenonx0710@gmail.com>
2023-08-01 13:15:45 -0500

    Added hyperlink on ingredient icon for Pokemon

--- src/components/shared/pokemon/ingredientIcon.tsx
+++ src/components/shared/pokemon/ingredientIcon.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import XMarkIcon from '@heroicons/react/24/solid/XMarkIcon';
 import Image from 'next/image';
 import {useTranslations} from 'next-intl';
+import Link from 'next-intl/link';
 
 import {imageSmallIconSizes} from '@/styles/image';
 import {classNames} from '@/utils/react';
@@ -25,8 +26,10 @@ export const PokemonIngredientIcon = ({id, dimension}: Props) => {
   }
 
   return (
-    <div className={classNames('relative', dimension ?? 'h-5 w-5')}>
-      <Image src={`/images/ingredient/${id}.png`} alt={t(id.toString())} fill sizes={imageSmallIconSizes}/>
-    </div>
+    <Link href={`/ingredient/${id}`} className="button-clickable">
+      <div className={classNames('relative', dimension ?? 'h-5 w-5')}>
+        <Image src={`/images/ingredient/${id}.png`} alt={t(id.toString())} fill sizes={imageSmallIconSizes}/>
+      </div>
+    </Link>
   );
 };
