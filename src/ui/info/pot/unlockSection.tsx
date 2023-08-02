d92951f9d0e16526cfed5197cd15d4cd8e11cdcf
RaenonX <raenonx0710@gmail.com>
2023-08-02 11:19:28 -0500

    Improved pot info page style

--- src/ui/info/pot/unlockSection.tsx
+++ src/ui/info/pot/unlockSection.tsx
@@ -25,32 +25,33 @@ export const PotRecipeUnlockSection = ({capacity, cumulativeCost, potInfo, meals
   const t = useTranslations('UI.InPage.Info.Pot');
 
   return (
-    <Flex direction="col" className="button-bg gap-1.5 rounded-lg p-2">
-      <Flex direction="row" className="gap-3" center>
-        <Flex direction="row" className="gap-1" center noFullWidth>
+    <Flex direction="col" className="button-bg gap-1.5 rounded-lg p-2 md:flex-row md:items-center">
+      <Flex direction="row" center noFullWidth className="gap-1.5 md:w-1/6 md:flex-col">
+        <Flex direction="row" center noFullWidth className="gap-1">
           <div className="relative h-7 w-7">
             <NextImage src="/images/generic/pot.png" alt={t('Capacity')} sizes={imageSmallIconSizes}/>
           </div>
           <div>{potInfo.capacity}</div>
         </Flex>
-        <Flex direction="row" className="gap-1" center noFullWidth>
+        <Flex direction="row" center noFullWidth className="gap-1">
           <div className="relative h-7 w-7">
             <NextImage src="/images/generic/shard.png" alt={t('Expand')} sizes={imageSmallIconSizes}/>
           </div>
-          <div>
-            {!capacity || capacity < potInfo.capacity ?
-              `${formatInt(cumulativeCost)} (+${formatInt(potInfo.cost)})` :
-              '-'}
-          </div>
+          {!capacity || capacity < potInfo.capacity ?
+            <Flex direction="row" className="gap-1 md:flex-col">
+              <div>{formatInt(cumulativeCost)}</div>
+              <div>(+{formatInt(potInfo.cost)})</div>
+            </Flex> :
+            <div>-</div>}
         </Flex>
-        <Flex direction="row" className="gap-1" center noFullWidth>
+        <Flex direction="row" center noFullWidth className="gap-1">
           <div className="relative h-7 w-7">
             <NextImage src="/images/generic/meal.png" alt={t('UnlockedRecipes')} sizes={imageSmallIconSizes}/>
           </div>
           <div>{unlockedRecipes} (+{meals.length})</div>
         </Flex>
       </Flex>
-      <HorizontalSplitter/>
+      <HorizontalSplitter className="block md:hidden"/>
       <Flex direction="row" wrap className="gap-1.5">
         {meals.length ?
           meals
