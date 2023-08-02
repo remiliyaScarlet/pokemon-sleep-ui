ede22935528c19d691dfd28752076f973fbffc66
RaenonX <raenonx0710@gmail.com>
2023-08-02 16:21:14 -0500

    Updated navbar style

--- src/ui/base/navbar/main.tsx
+++ src/ui/base/navbar/main.tsx
@@ -24,11 +24,11 @@ export const NavBar = () => {
       direction="row" center
       className="sticky top-0 z-50 gap-1.5 border-b border-b-gray-700 bg-slate-300/90 p-2 dark:bg-slate-900/90"
     >
+      <NavHomepage/>
       <Flex direction="row" className={classNames(
         'scrollbar-hide gap-1 overflow-x-auto overflow-y-hidden',
         'text-center text-sm text-gray-400',
       )}>
-        <NavHomepage/>
         {entries.map(({i18nTextId, ...props}) => (
           <div key={i18nTextId} className={styles['nav-height']}>
             <NavEntryUI alt={t(i18nTextId)} {...props}/>
