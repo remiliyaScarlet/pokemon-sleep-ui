73024c3b1272994f1bd13269a22e37b81058bcc8
RaenonX <raenonx0710@gmail.com>
2023-08-06 22:30:16 -0500

    Updated next.js `<Image/>` to use `unoptimized` instead #71

--- src/components/shared/common/image/main.tsx
+++ src/components/shared/common/image/main.tsx
@@ -4,7 +4,6 @@ import Image, {ImageProps} from 'next/image';
 
 import {imageCDN} from '@/const/website';
 import {isProduction} from '@/utils/environment';
-import {classNames} from '@/utils/react';
 
 
 export type NextImageProps = Omit<ImageProps, 'fill' | 'title'>;
@@ -12,19 +11,12 @@ export type NextImageProps = Omit<ImageProps, 'fill' | 'title'>;
 export const NextImage = ({src, alt, sizes, className}: NextImageProps) => {
   const actualSrc = isProduction() ? `${imageCDN}${src}` : src;
 
-  // Can't use `next.js` image optimization because it causes too much workload for the reverse proxy
-  if (isProduction()) {
-    return (
-      <picture>
-        <img
-          src={`${imageCDN}${src}`} alt={alt} title={alt} sizes={sizes}
-          className={classNames('absolute h-full w-full object-cover', className)}
-        />
-      </picture>
-    );
-  }
-
+  // Can't use `next.js` image optimization (therefore `unoptimized`)
+  // because it causes too much workload for the reverse proxy
   return (
-    <Image src={actualSrc} alt={alt} fill title={alt} sizes={sizes} className={className} objectFit="cover"/>
+    <Image
+      src={actualSrc} alt={alt} fill title={alt} sizes={sizes}
+      className={className} objectFit="cover" unoptimized
+    />
   );
 };
