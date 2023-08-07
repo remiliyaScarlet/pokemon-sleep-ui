5695550d1a6208dc9308276435ea9674679cc8b7
RaenonX <raenonx0710@gmail.com>
2023-08-07 05:46:03 -0500

    Fixed missing JP translation #74
    closes #74

---
+++ src/dummy/i18nCheck.ts
@@ -0,0 +1,15 @@
+// noinspection BadExpressionStatementJS
+
+/**
+ * This file is intend to trigger typescript json schema check only, therefore no export should happen here.
+ */
+import enTranslations from '../../messages/ui-en.json';
+import jaTranslations from '../../messages/ui-ja.json';
+import krTranslations from '../../messages/ui-kr.json';
+import zhTranslations from '../../messages/ui-zh.json';
+
+
+enTranslations satisfies IntlMessages['UI'];
+jaTranslations satisfies IntlMessages['UI'];
+krTranslations satisfies IntlMessages['UI'];
+zhTranslations satisfies IntlMessages['UI'];
