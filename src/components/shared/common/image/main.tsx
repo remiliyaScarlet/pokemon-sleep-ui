282623d18eb521b830097854ff36b6ef753569b8
RaenonX <raenonx0710@gmail.com>
2023-08-06 20:33:41 -0500

    Removed usage of `<NextImage>` for saving server load #71

--- src/components/shared/common/image/main.tsx
+++ src/components/shared/common/image/main.tsx
@@ -11,6 +11,13 @@ export type NextImageProps = Omit<ImageProps, 'fill' | 'title'>;
 export const NextImage = ({src, alt, sizes, className}: NextImageProps) => {
   const actualSrc = isProduction() ? `${imageCDN}${src}` : src;
 
+  if (isProduction()) {
+    return (
+      // eslint-disable-next-line @next/next/no-img-element
+      <img src={`${imageCDN}${src}`} alt={alt} title={alt} sizes={sizes} className={className}/>
+    );
+  }
+
   return (
     <Image src={actualSrc} alt={alt} fill title={alt} sizes={sizes} className={className}/>
   );
