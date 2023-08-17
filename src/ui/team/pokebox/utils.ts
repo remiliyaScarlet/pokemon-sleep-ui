855193f69d21e41df3580c29e9c32d66f0fd1689
RaenonX <raenonx0710@gmail.com>
2023-08-17 18:12:34 -0500

    Updated default ingredient quantity value in Pokebox

--- src/ui/team/pokebox/utils.ts
+++ src/ui/team/pokebox/utils.ts
@@ -1,5 +1,6 @@
 import {v4} from 'uuid';
 
+import {specialtyIdMap} from '@/const/game/pokemon';
 import {PokeInBox} from '@/types/game/pokebox';
 import {PokemonIngredientPick} from '@/types/game/producing/ingredient';
 import {IngredientId} from '@/types/mongo/ingredient';
@@ -10,19 +11,20 @@ import {isNotNullish} from '@/utils/type';
 const generatePokeInBoxIngredientSingle = (
   level: number,
   ingredientId: IngredientId | undefined,
+  quantity: number,
 ): PokemonIngredientPick | null => {
-  return ingredientId ? {level, id: ingredientId, quantity: 1} : null;
+  return ingredientId ? {level, id: ingredientId, quantity} : null;
 };
 
-export const generateNewPokeInBox = ({id, stats, ingredients}: PokemonInfo): PokeInBox => {
+export const generateNewPokeInBox = ({id, stats, ingredients, specialty}: PokemonInfo): PokeInBox => {
   return {
     uuid: v4(),
     pokemon: id,
     name: null,
     level: 1,
     randomIngredient: [
-      generatePokeInBoxIngredientSingle(30, ingredients.fixed),
-      generatePokeInBoxIngredientSingle(60, ingredients.fixed),
+      generatePokeInBoxIngredientSingle(30, ingredients.fixed, specialty === specialtyIdMap.ingredient ? 4 : 2),
+      generatePokeInBoxIngredientSingle(60, ingredients.fixed, specialty === specialtyIdMap.ingredient ? 7 : 3),
     ].filter(isNotNullish),
     carryLimit: stats.maxCarry,
     subSkill: {},
