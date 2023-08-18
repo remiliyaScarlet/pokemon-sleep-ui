28732a3d78acbfad1c8aa0ad38197b7f414b9412
RaenonX <raenonx0710@gmail.com>
2023-08-18 14:18:07 -0500

    Fixed linting issue

--- src/ui/team/pokebox/content/pokeInBox/grid/details/main.tsx
+++ src/ui/team/pokebox/content/pokeInBox/grid/details/main.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 
 import {PokeboxPokeInBoxInfo} from '@/ui/team/pokebox/content/pokeInBox/grid/details/info';
 import {PokeboxPokeInBoxProductionBerry} from '@/ui/team/pokebox/content/pokeInBox/grid/details/productionBerry';
-import {PokeboxPokeInBoxProductionIngredient} from '@/ui/team/pokebox/content/pokeInBox/grid/details/productionIngredient';
+import {
+  PokeboxPokeInBoxProductionIngredient,
+} from '@/ui/team/pokebox/content/pokeInBox/grid/details/productionIngredient';
 import {PokeboxPokeInBoxProductionTotal} from '@/ui/team/pokebox/content/pokeInBox/grid/details/productionTotal';
 import {PokeboxPokeInBoxSkills} from '@/ui/team/pokebox/content/pokeInBox/grid/details/skills';
 import {PokeboxPokeInBoxStats} from '@/ui/team/pokebox/content/pokeInBox/grid/details/stats';
