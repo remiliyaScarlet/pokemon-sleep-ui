0813d91d1f5c1bd7c96805a493485952f6081dca
RaenonX <raenonx0710@gmail.com>
2023-08-20 11:45:55 -0500

    Fixed potential layout shift when manually saving data

--- src/components/ads/wrapper.tsx
+++ src/components/ads/wrapper.tsx
@@ -10,8 +10,20 @@ import {isProduction} from '@/utils/environment';
 
 export const AdsWrapper = ({children, className}: React.PropsWithChildren<AdsUnitProps>) => {
   const {data, status} = useSession();
+  // Running `update()` of `useSession` puts the status to `loading`,
+  // which causes the ads to blink briefly for users with ads
+  // Therefore caching the ads-free status when the session loading is settled
+  const [isAdsFree, setIsAdsFree] = React.useState<boolean | null>(null);
 
-  if (status === 'loading' || (status === 'authenticated' && data?.user.isAdsFree)) {
+  React.useEffect(() => {
+    if (status === 'unauthenticated') {
+      setIsAdsFree(false);
+    } else if (status === 'authenticated' && data?.user.isAdsFree) {
+      setIsAdsFree(true);
+    }
+  }, [status]);
+
+  if (isAdsFree) {
     return <></>;
   }
 
