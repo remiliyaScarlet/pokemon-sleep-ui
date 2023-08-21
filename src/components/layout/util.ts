2d3faffec6e67520abc2c185c3ab6d90e95f5f57
RaenonX <raenonx0710@gmail.com>
2023-08-21 03:22:44 -0500

    Added `place-items-center` for grid

--- src/components/layout/util.ts
+++ src/components/layout/util.ts
@@ -4,7 +4,7 @@ import {LayoutProps} from '@/components/layout/type';
 
 
 export const getLayoutClassNames = ({center, stretch, noFullWidth, className}: LayoutProps) => clsx(
-  center && 'content-center items-center justify-center text-center',
+  center && 'place-items-center content-center items-center justify-center text-center',
   stretch && 'self-stretch',
   !noFullWidth && 'w-full',
   className,
