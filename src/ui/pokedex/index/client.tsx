remiliyaScarlet/pokemon-sleep-ui

commit e95388d35c72b4e627de1c9d1acd142d172224a1

Added Snorlax's favorite inclusion for berry production rate #100
closes #100

src/ui/pokedex/index/client.tsx
@@ -50,6 +50,7 @@ export const PokedexClient = (props: PokedexClientCommonProps) => {
     sort: filter.sort,
     ingredientMap,
     berryMap,
+    snorlaxFavorite: filter.snorlaxFavorite,
     triggerDeps: [filter],
     setLoading,
   });
@@ -61,7 +62,7 @@ export const PokedexClient = (props: PokedexClientCommonProps) => {
       <PokedexResultCount data={pokedex} inclusionMap={isIncluded}/>
       <LazyLoad loading={loading}>
         <Flex direction="row" wrap className="gap-1.5">
-          {sortedData.map(({source}) => (
+          {sortedData.map(({source, sorter}) => (
             <div
               key={source.pokemon.id}
               className={clsx(
@@ -74,8 +75,7 @@ export const PokedexClient = (props: PokedexClientCommonProps) => {
               <PokedexLink
                 pokemon={source.pokemon}
                 display={filter.display}
-                level={filter.level}
-                {...props}
+                sorter={sorter}
               />
             </div>
           ))}

src/ui/pokedex/index/linkDetail.tsx
@@ -13,18 +13,13 @@ import {PokemonSleepType} from '@/components/shared/pokemon/sleepType/main';
 import {PokemonSpecialty} from '@/components/shared/pokemon/specialty/main';
 import {imageSmallIconSizes} from '@/styles/image';
 import {PokedexLinkProps} from '@/ui/pokedex/index/type';
-import {getBerryProducingRate} from '@/utils/game/producing/berry';
-import {defaultNeutralOpts} from '@/utils/game/producing/const';
-import {getIngredientProducingRate} from '@/utils/game/producing/ingredient';
 import {formatFloat} from '@/utils/number';
 
 
 export const PokedexLinkDetail = React.memo(({
   pokemon,
   display,
-  level,
-  ingredientMap,
-  berryMap,
+  sorter,
 }: PokedexLinkProps) => {
   const {
     id,
@@ -33,7 +28,6 @@ export const PokedexLinkDetail = React.memo(({
     ingredients,
     specialty,
     sleepType,
-    stats,
   } = pokemon;
 
   const t = useTranslations('Game');
@@ -62,18 +56,18 @@ export const PokedexLinkDetail = React.memo(({
     return <PokemonIngredientIcons ingredients={ingredients}/>;
   }
 
-  if (display === 'specialty') {
-    return (
-      <Flex direction="row">
-        <PokemonSpecialty specialty={specialty}/>
-      </Flex>
-    );
-  }
-
   if (display === 'sleepType') {
     return (
       <Flex direction="row">
         <PokemonSleepType sleepType={sleepType}/>
+      </Flex>
+    );
+  }
+
+  if (display === 'specialty') {
+    return (
+      <Flex direction="row">
+        <PokemonSpecialty specialty={specialty}/>
       </Flex>
     );
   }
@@ -85,7 +79,7 @@ export const PokedexLinkDetail = React.memo(({
           <NextImage src="/images/generic/friendship.png" alt={t2('Stats.Friendship')} sizes={imageSmallIconSizes}/>
         </div>
         <div>
-          {stats.friendshipPoints}
+          {sorter}
         </div>
       </Flex>
     );
@@ -98,7 +92,7 @@ export const PokedexLinkDetail = React.memo(({
           <ClockIcon/>
         </div>
         <div>
-          {stats.frequency}
+          {sorter}
         </div>
       </Flex>
     );
@@ -119,91 +113,35 @@ export const PokedexLinkDetail = React.memo(({
       return <></>;
     }
 
-    const rate = getIngredientProducingRate({
-      level,
-      pokemon,
-      ingredient: ingredientMap[fixedIngredientId],
-      ...defaultNeutralOpts,
-    });
-
-    if (display === 'ingredientEnergy') {
-      return (
-        <Flex direction="row" className="gap-0.5">
-          <PokemonIngredientIcon id={fixedIngredientId}/>
-          <ColoredEnergyIcon alt={t2('Stats.Energy.Name')}/>
-          <div>
-            {formatFloat(rate?.dailyEnergy)}
-          </div>
-        </Flex>
-      );
-    }
-
-    if (display === 'ingredientCount') {
-      return (
-        <Flex direction="row" className="gap-0.5">
-          <PokemonIngredientIcon id={fixedIngredientId}/>
-          <div>
-            {formatFloat(rate?.quantity)}
-          </div>
-        </Flex>
-      );
-    }
+    return (
+      <Flex direction="row" className="gap-0.5">
+        <PokemonIngredientIcon id={fixedIngredientId}/>
+        {display === 'ingredientEnergy' && <ColoredEnergyIcon alt={t2('Stats.Energy.Name')}/>}
+        <div>
+          {formatFloat(sorter)}
+        </div>
+      </Flex>
+    );
   }
 
   if (display === 'berryEnergy' || display === 'berryCount') {
-    const rate = getBerryProducingRate({
-      level,
-      pokemon,
-      ...defaultNeutralOpts,
-      isSnorlaxFavorite: false,
-      berryData: berryMap[berry.id],
-    });
-
-    if (display === 'berryEnergy') {
-      return (
-        <Flex direction="row" className="gap-0.5">
-          <PokemonBerryIcon id={berry.id}/>
-          <ColoredEnergyIcon alt={t2('Stats.Energy.Name')}/>
-          <div>
-            {formatFloat(rate?.dailyEnergy)}
-          </div>
-        </Flex>
-      );
-    }
-
-    if (display === 'berryCount') {
-      return (
-        <Flex direction="row" className="gap-0.5">
-          <PokemonBerryIcon id={berry.id}/>
-          <div>
-            {formatFloat(rate?.quantity)}
-          </div>
-        </Flex>
-      );
-    }
+    return (
+      <Flex direction="row" className="gap-0.5">
+        <PokemonBerryIcon id={berry.id}/>
+        {display === 'berryEnergy' && <ColoredEnergyIcon alt={t2('Stats.Energy.Name')}/>}
+        <div>
+          {formatFloat(sorter)}
+        </div>
+      </Flex>
+    );
   }
 
   if (display === 'totalEnergy') {
-    const rateOfBerry = getBerryProducingRate({
-      level,
-      pokemon,
-      ...defaultNeutralOpts,
-      isSnorlaxFavorite: false,
-      berryData: berryMap[berry.id],
-    });
-
-    const rateOfIngredient = getIngredientProducingRate({
-      level,
-      pokemon,
-      ingredient: ingredients.fixed ? ingredientMap[ingredients.fixed] : undefined,
-      ...defaultNeutralOpts,
-    });
-
     return (
       <Flex direction="row" className="gap-0.5">
         <ColoredEnergyIcon alt={t2('Stats.Energy.Name')}/>
         <div>
-          {formatFloat(rateOfBerry.dailyEnergy + (rateOfIngredient?.dailyEnergy ?? 0))}
+          {formatFloat(sorter)}
         </div>
       </Flex>
     );

src/ui/pokedex/index/type.ts
@@ -3,8 +3,10 @@ import {Session} from 'next-auth';
 import {FilterInclusionMap} from '@/components/input/filter/type';
 import {PokemonInputFilter} from '@/components/shared/pokemon/input/type';
 import {PokemonSortType} from '@/components/shared/pokemon/sorter/type';
+import {SnorlaxFavorite} from '@/types/game/snorlax';
 import {BerryDataMap} from '@/types/mongo/berry';
 import {IngredientMap} from '@/types/mongo/ingredient';
+import {FieldMetaMap} from '@/types/mongo/mapMeta';
 import {PokemonInfo} from '@/types/mongo/pokemon';
 import {SleepMapId, SleepStyleData} from '@/types/mongo/sleepStyle';
 import {PokedexDisplayType} from '@/ui/pokedex/index/input/type';
@@ -26,6 +28,7 @@ export type PokedexFilter = PokemonInputFilter & PokedexDisplay & {
   name: string,
   mapId: FilterInclusionMap<SleepMapId>,
   level: number,
+  snorlaxFavorite: SnorlaxFavorite,
 };
 
 export type PokedexClientCommonProps = {
@@ -33,9 +36,11 @@ export type PokedexClientCommonProps = {
   maxLevel: number,
   ingredientMap: IngredientMap,
   berryMap: BerryDataMap,
+  mapMeta: FieldMetaMap,
   session: Session | null,
 };
 
-export type PokedexLinkProps = Pick<PokedexFilter, 'display' | 'level'> & PokedexClientCommonProps & {
+export type PokedexLinkProps = Pick<PokedexFilter, 'display'> & {
   pokemon: PokemonInfoForPokedex,
+  sorter: number,
 };

src/components/input/filter/type.ts
@@ -9,7 +9,7 @@ export type InputRowProps = {
 
 export type FilterInclusionMap<TId extends Indexable> = {[id in TId]?: boolean};
 
-export type FilterWithInclusionMap<TId extends Indexable> = Record<TId, FilterInclusionMap<TId> | any>;
+export type FilterWithInclusionMap<TId extends Indexable> = Record<string, FilterInclusionMap<TId> | any>;
 
 export type FilterInputProps<TFilter> = {
   filter: TFilter,

src/components/shared/pokemon/sorter/type.ts
@@ -1,5 +1,6 @@
 import {PokemonIngredientPick} from '@/types/game/producing/ingredient';
 import {ProducingRateSingleParams} from '@/types/game/producing/rate';
+import {SnorlaxFavorite} from '@/types/game/snorlax';
 import {BerryData} from '@/types/mongo/berry';
 import {IngredientMap} from '@/types/mongo/ingredient';
 import {PokemonInfo} from '@/types/mongo/pokemon';
@@ -36,6 +37,7 @@ export type PokemonSorterGetterOpts = ProducingRateSingleParams & {
   ingredients: PokemonIngredientPick[],
   ingredientMap: IngredientMap,
   berryData: BerryData | null,
+  snorlaxFavorite: SnorlaxFavorite,
 };
 
 export type PokemonSorterGetter = (opts: PokemonSorterGetterOpts) => number;

src/ui/analysis/page/calc/type.ts
@@ -1,4 +1,5 @@
 import {SnorlaxRank} from '@/types/game/rank';
+import {SnorlaxFavorite} from '@/types/game/snorlax';
 import {BerryId} from '@/types/mongo/berry';
 import {IngredientId} from '@/types/mongo/ingredient';
 import {
@@ -73,6 +74,7 @@ export type GetAnalysisStatsCommonOpts<TSample> = {
   getPokemonId: (sample: TSample) => PokemonId,
 };
 
-export type GetAnalysisStatsOpts = AnalysisPageCommonProps & {
+export type GetAnalysisStatsOpts = Omit<AnalysisPageCommonProps, 'mapMeta'> & {
   level: number,
+  snorlaxFavorite: SnorlaxFavorite,
 };

src/ui/analysis/page/client.tsx
@@ -29,6 +29,7 @@ export const AnalysisPageClient = (props: AnalysisPageCommonProps) => {
     ...props,
     level: filter.level,
     pokemonToAnalyze: pokedex.filter(({id}) => isIncluded[id]),
+    snorlaxFavorite: filter.snorlaxFavorite,
     setStats,
     setLoading,
     calculateDeps: [filter],

src/ui/team/analysis/type.ts
@@ -1,17 +1,15 @@
-import {FilterInclusionMap} from '@/components/input/filter/type';
 import {PokemonInputFilter} from '@/components/shared/pokemon/input/type';
 import {PokemonSubSkill, SubSkillMap} from '@/types/game/pokemon/subskill';
 import {NatureId} from '@/types/game/producing/nature';
+import {SnorlaxFavorite} from '@/types/game/snorlax';
 import {Migratable} from '@/types/migrate';
-import {BerryDataMap, BerryId} from '@/types/mongo/berry';
+import {BerryDataMap} from '@/types/mongo/berry';
 import {IngredientMap} from '@/types/mongo/ingredient';
 import {FieldMetaMap} from '@/types/mongo/mapMeta';
 import {PokedexMap, PokemonId} from '@/types/mongo/pokemon';
 import {SnorlaxRankInMap} from '@/types/mongo/snorlax';
 import {UserLazyLoadedData} from '@/types/userData/main';
 
-
-export type SnorlaxFavorite = FilterInclusionMap<BerryId>;
 
 export type TeamAnalysisFilter = PokemonInputFilter & {
   snorlaxFavorite: SnorlaxFavorite,

commit f6650b662f527681c1b88ddc55a895e3ef2b4eff

Fixed incorrect Pokédex display introduced in e95388d3

src/ui/pokedex/index/client.tsx
@@ -64,13 +64,15 @@ export const PokedexClient = (props: PokedexClientCommonProps) => {
         <Grid className={clsx(
           'grid-cols-2 gap-1.5 xs:grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-8',
         )}>
-          {sortedData.map(({source, sorter}) => (
+          {sortedData.map(({source}) => (
             isIncluded[source.pokemon.id] ?
               <PokedexLink
                 key={source.pokemon.id}
                 pokemon={source.pokemon}
                 display={filter.display}
-                sorter={sorter}
+                level={filter.level}
+                snorlaxFavorite={filter.snorlaxFavorite}
+                {...props}
               /> :
               <React.Fragment key={source.pokemon.id}/>
           ))}

src/ui/pokedex/index/linkDetail.tsx
@@ -10,16 +10,22 @@ import {PokemonBerryIcon} from '@/components/shared/pokemon/berry/icon';
 import {PokemonIngredientIcon} from '@/components/shared/pokemon/ingredients/icon';
 import {PokemonIngredientIcons} from '@/components/shared/pokemon/ingredients/icons';
 import {PokemonSleepType} from '@/components/shared/pokemon/sleepType/main';
+import {getPokemonSorter} from '@/components/shared/pokemon/sorter/calc';
 import {PokemonSpecialty} from '@/components/shared/pokemon/specialty/main';
+import {specialtyIdMap} from '@/const/game/pokemon';
 import {imageSmallIconSizes} from '@/styles/image';
 import {PokedexLinkProps} from '@/ui/pokedex/index/type';
+import {defaultNeutralOpts} from '@/utils/game/producing/const';
 import {formatFloat} from '@/utils/number';
 
 
 export const PokedexLinkDetail = React.memo(({
   pokemon,
   display,
-  sorter,
+  level,
+  ingredientMap,
+  berryMap,
+  snorlaxFavorite,
 }: PokedexLinkProps) => {
   const {
     id,
@@ -72,6 +78,21 @@ export const PokedexLinkDetail = React.memo(({
     );
   }
 
+  const sorterOfDisplay = getPokemonSorter({
+    type: display,
+    pokemon,
+    berryMap,
+    ingredientMap,
+    ingredients: (
+      ingredients.fixed ?
+        [{level, id: ingredients.fixed, quantity: specialty === specialtyIdMap.ingredient ? 2 : 1}] :
+        []
+    ),
+    level,
+    snorlaxFavorite,
+    ...defaultNeutralOpts,
+  });
+
   if (display === 'friendshipPoint') {
     return (
       <Flex direction="row" className="gap-0.5">
@@ -79,7 +100,7 @@ export const PokedexLinkDetail = React.memo(({
           <NextImage src="/images/generic/friendship.png" alt={t2('Stats.Friendship')} sizes={imageSmallIconSizes}/>
         </div>
         <div>
-          {sorter}
+          {sorterOfDisplay}
         </div>
       </Flex>
     );
@@ -92,7 +113,7 @@ export const PokedexLinkDetail = React.memo(({
           <ClockIcon/>
         </div>
         <div>
-          {sorter}
+          {sorterOfDisplay}
         </div>
       </Flex>
     );
@@ -118,7 +139,7 @@ export const PokedexLinkDetail = React.memo(({
         <PokemonIngredientIcon id={fixedIngredientId}/>
         {display === 'ingredientEnergy' && <ColoredEnergyIcon alt={t2('Stats.Energy.Name')}/>}
         <div>
-          {formatFloat(sorter)}
+          {formatFloat(sorterOfDisplay)}
         </div>
       </Flex>
     );
@@ -130,7 +151,7 @@ export const PokedexLinkDetail = React.memo(({
         <PokemonBerryIcon id={berry.id}/>
         {display === 'berryEnergy' && <ColoredEnergyIcon alt={t2('Stats.Energy.Name')}/>}
         <div>
-          {formatFloat(sorter)}
+          {formatFloat(sorterOfDisplay)}
         </div>
       </Flex>
     );
@@ -141,7 +162,7 @@ export const PokedexLinkDetail = React.memo(({
       <Flex direction="row" className="gap-0.5">
         <ColoredEnergyIcon alt={t2('Stats.Energy.Name')}/>
         <div>
-          {formatFloat(sorter)}
+          {formatFloat(sorterOfDisplay)}
         </div>
       </Flex>
     );

src/ui/pokedex/index/type.ts
@@ -40,7 +40,7 @@ export type PokedexClientCommonProps = {
   session: Session | null,
 };
 
-export type PokedexLinkProps = Pick<PokedexFilter, 'display'> & {
+export type PokedexLinkProps = Pick<PokedexFilter, 'display' | 'level'> & PokedexClientCommonProps & {
   pokemon: PokemonInfoForPokedex,
-  sorter: number,
+  snorlaxFavorite: SnorlaxFavorite,
 };