9c8461b8492781da9c46ea04a3a8550e1d8a5527
RaenonX <raenonx0710@gmail.com>
2023-08-26 02:52:52 -0500

    Added `permutation` generator

--- src/utils/compute.ts
+++ src/utils/compute.ts
@@ -7,3 +7,26 @@ export function* cartesianIterator<T>(items: T[][]): Generator<T[]> {
     }
   }
 }
+
+// https://stackoverflow.com/a/37580979/11571888
+export function* permuteIterator<T>(permutation: T[]): Generator<T[]> {
+  const length = permutation.length;
+  const c = Array(length).fill(0);
+  let i = 1; let k; let p;
+
+  yield permutation.slice();
+  while (i < length) {
+    if (c[i] < i) {
+      k = i % 2 && c[i];
+      p = permutation[i];
+      permutation[i] = permutation[k];
+      permutation[k] = p;
+      ++c[i];
+      i = 1;
+      yield permutation.slice();
+    } else {
+      c[i] = 0;
+      ++i;
+    }
+  }
+}
