f4626a7e634968bcf56a57de814eab076a611695
RaenonX <raenonx0710@gmail.com>
2023-08-27 16:53:09 -0500

    Added `ref` for `<Grid/>`

--- src/components/layout/grid.tsx
+++ src/components/layout/grid.tsx
@@ -8,13 +8,17 @@ import {getLayoutClassNames} from '@/components/layout/util';
 
 type Props = LayoutProps;
 
-export const Grid = ({children, ...props}: React.PropsWithChildren<Props>) => {
+export const Grid = React.forwardRef<HTMLDivElement, React.PropsWithChildren<Props>>(({
+  children,
+  ...props
+}, ref) => {
   return (
-    <div className={clsx(
+    <div ref={ref} className={clsx(
       'grid',
       getLayoutClassNames(props),
     )}>
       {children}
     </div>
   );
-};
+});
+Grid.displayName = 'Grid';
