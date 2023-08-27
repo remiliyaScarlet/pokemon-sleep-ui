3a72191a89fbb20661f96e2cb28ef598b603036f
RaenonX <raenonx0710@gmail.com>
2023-08-27 15:26:04 -0500

    Added `dimension` for Pokemon data icon

--- src/components/shared/pokemon/dataIcon.tsx
+++ src/components/shared/pokemon/dataIcon.tsx
@@ -4,17 +4,19 @@ import {clsx} from 'clsx';
 
 import {NextImage} from '@/components/shared/common/image/main';
 import {imageSmallIconSizes} from '@/styles/image';
+import {Dimension} from '@/types/style';
 
 
 type Props = {
   src: string,
   alt: string,
   invert?: boolean,
+  dimension?: Dimension,
 };
 
-export const PokemonDataIcon = ({src, alt, invert}: Props) => {
+export const PokemonDataIcon = ({src, alt, invert, dimension}: Props) => {
   return (
-    <div className="relative h-6 w-6">
+    <div className={clsx('relative', dimension ?? 'h-6 w-6')}>
       <NextImage
         src={src} alt={alt} sizes={imageSmallIconSizes}
         className={clsx(invert && 'invert-hoverable')}
