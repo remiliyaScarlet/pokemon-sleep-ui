299ea12ef7c0435586d574074d9cc34f7a364cb0
RaenonX <raenonx0710@gmail.com>
2023-08-27 14:05:35 -0500

    Refactored user external sign in page

--- src/ui/auth/signIn/external.tsx
+++ src/ui/auth/signIn/external.tsx
@@ -1,25 +1,14 @@
 'use client';
 import React from 'react';
 
-import ArrowTopRightOnSquareIcon from '@heroicons/react/24/outline/ArrowTopRightOnSquareIcon';
-import {signIn} from 'next-auth/react';
-
 import {Flex} from '@/components/layout/flex';
+import {AuthSignInExternalProvider} from '@/ui/auth/signIn/provider';
 
 
 export const AuthSignInExternal = () => {
   return (
     <Flex direction="col" center className="info-section">
-      <button className="button-clickable-bg w-1/2 p-2 md:w-1/3" onClick={() => signIn('google')}>
-        <Flex direction="row" noFullWidth center className="gap-1.5">
-          <div className="h-6 w-6">
-            <ArrowTopRightOnSquareIcon/>
-          </div>
-          <div>
-            Google
-          </div>
-        </Flex>
-      </button>
+      <AuthSignInExternalProvider provider="google" text="Google"/>
     </Flex>
   );
 };
---
+++ src/ui/auth/signIn/provider.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import ArrowTopRightOnSquareIcon from '@heroicons/react/24/outline/ArrowTopRightOnSquareIcon';
+import {signIn} from 'next-auth/react';
+
+import {Flex} from '@/components/layout/flex';
+
+
+type Props = {
+  provider: string,
+  text: React.ReactNode,
+};
+
+export const AuthSignInExternalProvider = ({provider, text}: Props) => {
+  return (
+    <button className="button-clickable-bg w-1/2 p-2 md:w-1/3" onClick={() => signIn(provider)}>
+      <Flex direction="row" noFullWidth center className="gap-1.5">
+        <div className="h-6 w-6">
+          <ArrowTopRightOnSquareIcon/>
+        </div>
+        <div>
+          {text}
+        </div>
+      </Flex>
+    </button>
+  );
+};
