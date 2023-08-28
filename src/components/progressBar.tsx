96841f50b7535580d10f7021334ddf387aa0d634
RaenonX <raenonx0710@gmail.com>
2023-08-28 16:22:46 -0500

    Added `className` in progress bar

--- src/components/progressBar.tsx
+++ src/components/progressBar.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 
+import {clsx} from 'clsx';
+
 
 type Props = {
   percent: number,
+  className?: string,
 };
 
-export const ProgressBar = ({percent}: Props) => {
+export const ProgressBar = ({percent, className}: Props) => {
   return (
-    <div className="transform-smooth h-2.5 w-full rounded-full bg-gray-400/50 dark:bg-gray-700/50">
+    <div className={clsx('transform-smooth h-2.5 w-full rounded-full bg-gray-400/50 dark:bg-gray-700/50', className)}>
       <div className="transform-smooth h-2.5 rounded-full bg-slate-500" style={{width: `${percent}%`}}/>
     </div>
   );
