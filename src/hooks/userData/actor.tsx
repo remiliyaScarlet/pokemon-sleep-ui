7f6a462a96af083ac9759552f403b0061c89acf7
RaenonX <raenonx0710@gmail.com>
2023-08-31 16:02:51 -0500

    Added toast popup when user data actor got `failed` status #133
    closes #133

--- src/hooks/userData/actor.tsx
+++ src/hooks/userData/actor.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import ExclamationCircleIcon from '@heroicons/react/24/outline/ExclamationCircleIcon';
 import {useSession} from 'next-auth/react';
 
 import {useOverridableSession} from '@/hooks/session';
 import {UserDataActionStatus, UserDataActor} from '@/types/userData/main';
+import {showToast} from '@/utils/toast';
 
 
 type UseUserDataActorReturn = {
@@ -29,6 +31,14 @@ export const useUserDataActor = (override?: ReturnType<typeof useSession>): UseU
   React.useEffect(() => {
     if (status !== 'completed' && status !== 'failed') {
       return;
+    }
+
+    if (status === 'failed') {
+      showToast({content: (
+        <div className="relative h-9 w-9">
+          <ExclamationCircleIcon/>
+        </div>
+      )});
     }
 
     const timeoutId = setTimeout(() => setStatus('waiting'), 2500);
