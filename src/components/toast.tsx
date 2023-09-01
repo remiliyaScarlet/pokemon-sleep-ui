5e0121ade1e09c632fa6888a6664cd40d696b73c
RaenonX <raenonx0710@gmail.com>
2023-08-31 19:56:57 -0500

    Updated toast background

--- src/components/toast.tsx
+++ src/components/toast.tsx
@@ -11,7 +11,7 @@ export const Toast = ({visible, children}: React.PropsWithChildren<Props>) => {
   return (
     <div className={clsx(
       'rounded-full px-6 py-4',
-      'bg-slate-700/90 text-slate-300 dark:bg-slate-400/90 dark:text-slate-800',
+      'bg-slate-700/90 text-slate-300 dark:bg-slate-100/90 dark:text-slate-800',
       visible ? 'animate-enter' : 'animate-leave',
     )}>
       {children}
