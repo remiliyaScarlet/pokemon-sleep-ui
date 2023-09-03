bafc4166f9138393166ebacea5285a2d8468a7a8
RaenonX <raenonx0710@gmail.com>
2023-09-03 01:18:42 -0500

    Added soft anti-adblock #204
    closes #204

--- src/components/ads/const.ts
+++ src/components/ads/const.ts
@@ -1,3 +1,18 @@
+import {Locale} from '@/types/next/locale';
+
+
 export const adsClientId = 'ca-pub-1535004092052078';
 
 export const adsHeight = 'h-52 lg:h-32';
+
+export const adsMessage: {[locale in Locale]: string} = {
+  zh: '請解除封鎖廣告，或到關於頁面 (/about) 中的 Discord / Patreon 訂閱網站 (US$3.33+) 以啟用無廣告。',
+  en: 'Please allow ads, or check Discord/Patreon in the about page (/about) ' +
+    'for subscription (US$3.33+) to activate ads-free.',
+  ja: 'Please allow ads, or check Discord/Patreon in the about page (/about) ' +
+    'for subscription (US$3.33+) to activate ads-free.',
+  kr: 'Please allow ads, or check Discord/Patreon in the about page (/about) ' +
+    'for subscription (US$3.33+) to activate ads-free.',
+  de: 'Please allow ads, or check Discord/Patreon in the about page (/about) ' +
+    'for subscription (US$3.33+) to activate ads-free.',
+};
--- src/components/ads/wrapper.tsx
+++ src/components/ads/wrapper.tsx
@@ -2,9 +2,13 @@ import React from 'react';
 
 import {clsx} from 'clsx';
 import {useSession} from 'next-auth/react';
+import {useLocale} from 'next-intl';
 
-import {adsHeight} from '@/components/ads/const';
+import {adsHeight, adsMessage} from '@/components/ads/const';
 import {AdsUnitProps} from '@/components/ads/type';
+import {Flex} from '@/components/layout/flex';
+import {defaultLocale} from '@/const/website';
+import {Locale} from '@/types/next/locale';
 import {isProduction} from '@/utils/environment';
 
 
@@ -14,6 +18,7 @@ export const AdsWrapper = ({children, className}: React.PropsWithChildren<AdsUni
   // which causes the ads to blink briefly for users with ads
   // Therefore caching the ads-free status when the session loading is settled
   const [isAdsFree, setIsAdsFree] = React.useState<boolean | null>(null);
+  const locale = useLocale();
 
   React.useEffect(() => {
     if (status === 'unauthenticated') {
@@ -29,12 +34,17 @@ export const AdsWrapper = ({children, className}: React.PropsWithChildren<AdsUni
 
   return (
     <div className={clsx(
-      'w-full overflow-auto',
+      'relative w-full overflow-auto',
       className,
       adsHeight,
-      !isProduction() && 'border border-green-500',
+      isProduction() ? 'text-red-600 dark:text-red-400' : 'border border-green-500',
     )}>
-      {children}
+      <Flex direction="col" center className="h-full text-xl">
+        {adsMessage[locale as Locale] ?? adsMessage[defaultLocale]}
+      </Flex>
+      <div className="absolute left-0 top-0 h-full w-full">
+        {children}
+      </div>
     </div>
   );
 };
