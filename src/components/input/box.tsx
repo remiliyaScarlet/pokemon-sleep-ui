2486a042bbe267b21bc58cbcf1ffc977890cec84
RaenonX <raenonx0710@gmail.com>
2023-09-21 23:56:56 -0500

    Added global `autoFocus` disabling for input box #252
    closes #252

--- src/components/input/box.tsx
+++ src/components/input/box.tsx
@@ -8,6 +8,7 @@ type Props = React.InputHTMLAttributes<HTMLInputElement>;
 export const InputBox = ({className, ...props}: Props) => (
   <input
     className={clsx('border-b border-gray-700 bg-transparent focus:outline-none', className)}
+    autoFocus={false}
     {...props}
   />
 );
