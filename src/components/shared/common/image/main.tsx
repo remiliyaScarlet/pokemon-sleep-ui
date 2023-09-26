c01081a460c6315f4b63b85b7698cb4e7124b612
RaenonX <raenonx0710@gmail.com>
2023-09-26 05:14:06 -0500

    Added `ref` for `<NextImage/>`

--- src/components/shared/common/image/main.tsx
+++ src/components/shared/common/image/main.tsx
@@ -6,8 +6,12 @@ import Image, {ImageProps} from 'next/image';
 
 export type NextImageProps = Omit<ImageProps, 'fill' | 'title'>;
 
-export const NextImage = ({src, alt, sizes, className}: NextImageProps) => {
-  return (
-    <Image src={src} alt={alt} fill title={alt} sizes={sizes} className={clsx('object-cover', className)}/>
-  );
-};
+export const NextImage = React.forwardRef<HTMLImageElement, React.PropsWithChildren<NextImageProps>>(({
+  src,
+  alt,
+  sizes,
+  className,
+}, ref) => (
+  <Image ref={ref} src={src} alt={alt} fill title={alt} sizes={sizes} className={clsx('object-cover', className)}/>
+));
+NextImage.displayName = 'NextImage';
