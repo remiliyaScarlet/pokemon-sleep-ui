fb2d30922388dd37418264286592929c7a9f6bad
RaenonX <raenonx0710@gmail.com>
2023-10-02 22:34:22 -0500

    Added navbar home button theming

--- src/ui/base/navbar/list/entry/home.tsx
+++ src/ui/base/navbar/list/entry/home.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-import {clsx} from 'clsx';
 import {useTranslations} from 'next-intl';
 import Link from 'next-intl/link';
 
+import {Flex} from '@/components/layout/flex/common';
 import {NextImage} from '@/components/shared/common/image/main';
-import {imageSmallIconSizes} from '@/styles/image';
+import {imageGallerySizes, imageSmallIconSizes} from '@/styles/image';
 
 
 export const NavListHome = () => {
@@ -14,15 +14,21 @@ export const NavListHome = () => {
   const rankBallId = Date.now() % 4 + 1;
 
   return (
-    <Link href="/" className={clsx(
-      'button-clickable-bg flex flex-col items-center justify-center gap-1 rounded-lg p-2',
-    )}>
-      <div className="relative h-10 w-10">
-        <NextImage src={`/images/rank/${rankBallId}.png`} alt={t('Home.Title')} sizes={imageSmallIconSizes}/>
+    <Link href="/" className="button-clickable-bg group">
+      <div className="relative h-48 w-full">
+        <NextImage
+          src="/images/theme/3iggs.png" alt="Nav Theme"
+          sizes={imageGallerySizes} className="rounded-xl opacity-50 dark:opacity-25"
+        />
       </div>
-      <div className="text-center">
-        {t('Site.Name')}
-      </div>
+      <Flex center className="absolute left-0 top-0 z-10 h-full gap-1.5">
+        <div className="relative h-12 w-12">
+          <NextImage src={`/images/rank/${rankBallId}.png`} alt={t('Home.Title')} sizes={imageSmallIconSizes}/>
+        </div>
+        <div className="text-center">
+          {t('Site.Name')}
+        </div>
+      </Flex>
     </Link>
   );
 };
