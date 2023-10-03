ca4e44b24f973ffa21ab1a603ee98368e2a818d0
RaenonX <raenonx0710@gmail.com>
2023-10-02 21:04:07 -0500

    Updated nav list home/entry separator to ads

--- src/ui/base/navbar/list/content.tsx
+++ src/ui/base/navbar/list/content.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
+import {AdsUnit} from '@/components/ads/main';
 import {Flex} from '@/components/layout/flex/common';
-import {HorizontalSplitter} from '@/components/shared/common/splitter';
 import {useNavEntries} from '@/hooks/nav';
 import {NavListHome} from '@/ui/base/navbar/list/entry/home';
 import {NavListEntry} from '@/ui/base/navbar/list/entry/main';
@@ -13,7 +13,7 @@ export const NavListContent = () => {
   return (
     <Flex className="info-section-opaque h-full gap-1.5 overflow-y-scroll p-6">
       <NavListHome/>
-      <HorizontalSplitter/>
+      <AdsUnit alwaysSingle/>
       {entries.map((entry) => <NavListEntry key={entry.i18nTextId} entry={entry}/>)}
     </Flex>
   );
