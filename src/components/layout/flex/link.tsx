298361ffa4bfa1297d8087a6bf57d7e9ab0497c5
RaenonX <raenonx0710@gmail.com>
2023-10-02 20:12:55 -0500

    Added `<FlexLink/>`

---
+++ src/components/layout/flex/link.tsx
@@ -0,0 +1,26 @@
+import {UrlObject} from 'url';
+
+import React from 'react';
+
+import Link from 'next-intl/link';
+
+import {FlexCommonProps} from '@/components/layout/flex/type';
+import {getFlexStyles} from '@/components/layout/flex/utils';
+
+
+type Props = FlexCommonProps & {
+  href: string | UrlObject,
+};
+
+export const FlexLink = React.forwardRef<HTMLAnchorElement, React.PropsWithChildren<Props>>(({
+  direction = 'row',
+  noFullWidth = true,
+  href,
+  children,
+  ...props
+}, ref) => (
+  <Link ref={ref} href={href} className={getFlexStyles({direction, noFullWidth, ...props})}>
+    {children}
+  </Link>
+));
+FlexLink.displayName = 'FlexLink';
