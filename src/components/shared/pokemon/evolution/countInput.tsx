cada73dbb2f2a53e0d19978fd3635d8fe8b24d27
RaenonX <raenonx0710@gmail.com>
2023-10-03 03:11:37 -0500

    Added evolution count input

---
+++ src/components/shared/pokemon/evolution/countInput.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import {useTranslations} from 'next-intl';
+
+import {FilterTextInput} from '@/components/input/filter/text';
+import {Flex} from '@/components/layout/flex/common';
+import {PokemonDataIcon} from '@/components/shared/pokemon/dataIcon';
+
+
+type Props = {
+  idPrefix: string,
+  evolutionCount: number,
+  maxEvolutionCount: number,
+  setEvolutionCount: (evolutionCount: number) => void,
+};
+
+export const PokemonEvolutionCountInput = ({
+  idPrefix,
+  evolutionCount,
+  maxEvolutionCount,
+  setEvolutionCount,
+}: Props) => {
+  const t = useTranslations('UI.InPage.Pokedex');
+
+  const text = t('Input.EvolutionCount');
+
+  return (
+    <FilterTextInput
+      noFixedTitleWidth
+      title={
+        <Flex direction="row" className="items-center justify-end gap-0.5 px-2">
+          <PokemonDataIcon src="/images/generic/flash.png" alt={text} invert/>
+          <div>{text}</div>
+        </Flex>
+      }
+      idToItemId={(id) => `${idPrefix}-EvolutionStage-${id}`}
+      idToButton={(id) => <div className="mx-1">{id}</div>}
+      ids={[...new Array(maxEvolutionCount).keys()]}
+      onClick={setEvolutionCount}
+      isActive={(id) => evolutionCount === id}
+    />
+  );
+};
