19729818bb714dac9579a2ab94735c4edf756ba1
RaenonX <raenonx0710@gmail.com>
2023-10-04 17:21:47 -0500

    Updated default anchor ads visibility

--- src/components/ads/anchor.tsx
+++ src/components/ads/anchor.tsx
@@ -4,10 +4,11 @@ import React from 'react';
 import {AdsUnit} from '@/components/ads/main';
 import {useCollapsible} from '@/components/layout/collapsible/hook';
 import {Collapsible} from '@/components/layout/collapsible/main';
+import {isProduction} from '@/utils/environment';
 
 
 export const AnchorAdsUnit = () => {
-  const ads = useCollapsible(true);
+  const ads = useCollapsible(isProduction());
 
   return (
     <Collapsible state={ads} button="Ads" classNameForHeight="h-32">
