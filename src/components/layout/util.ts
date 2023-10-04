24c4aefcb814cd517b6ae8b55fc8ab76c84fed61
RaenonX <raenonx0710@gmail.com>
2023-10-04 12:10:36 -0500

    Updated layout `stretch` logic

--- src/components/layout/util.ts
+++ src/components/layout/util.ts
@@ -5,7 +5,7 @@ import {LayoutProps} from '@/components/layout/type';
 
 export const getLayoutClassNames = ({center, stretch, noFullWidth, className}: LayoutProps) => clsx(
   center && 'place-content-center place-items-center items-center text-center',
-  stretch && 'self-stretch',
+  stretch && 'items-stretch justify-stretch self-stretch',
   !noFullWidth && 'w-full',
   className,
 );
