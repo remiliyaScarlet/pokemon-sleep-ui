0101fd0e563d1fac57bd63498a5fbe473b287296
RaenonX <raenonx0710@gmail.com>
2023-10-05 14:49:53 -0500

    Attempting to fix iOS menu icon disappearance issue

--- src/ui/base/navbar/list/button.tsx
+++ src/ui/base/navbar/list/button.tsx
@@ -2,14 +2,13 @@ import React from 'react';
 
 import Bars3Icon from '@heroicons/react/24/outline/Bars3Icon';
 
-import {FlexButton} from '@/components/layout/flex/button';
 import {NavListCommonProps} from '@/ui/base/navbar/list/type';
 
 
 export const NavListButton = ({setOpen}: NavListCommonProps) => {
   return (
-    <FlexButton onClick={() => setOpen(true)} center className="button-clickable h-8 w-8 p-0.5">
+    <button onClick={() => setOpen(true)} className="button-clickable nav-height w-8 p-1">
       <Bars3Icon/>
-    </FlexButton>
+    </button>
   );
 };
