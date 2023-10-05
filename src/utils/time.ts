19cacc30387727d15c8326f732649c52931494a4
RaenonX <raenonx0710@gmail.com>
2023-10-05 08:02:06 -0500

    Added handling of special values in `formatSeconds()`

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -20,6 +20,18 @@ export const toSeconds = (time: string): number => {
 };
 
 export const formatSeconds = (seconds: number): string => {
+  if (seconds === Infinity) {
+    return '∞';
+  }
+
+  if (seconds === -Infinity) {
+    return '-∞';
+  }
+
+  if (isNaN(seconds)) {
+    return 'NaN';
+  }
+
   const h = Math.floor(seconds / 3600).toString();
   const m = Math.floor(seconds % 3600 / 60).toString().padStart(2, '0');
   const s = Math.floor(seconds % 3600 % 60).toString().padStart(2, '0');
