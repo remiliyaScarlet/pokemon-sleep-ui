bdf746b6126b795b6966cdcda37037c47c4a5600
RaenonX <raenonx0710@gmail.com>
2023-10-07 19:08:38 -0500

    Updated analysis page breakpoint

--- src/ui/analysis/page/stats/layout.tsx
+++ src/ui/analysis/page/stats/layout.tsx
@@ -5,7 +5,7 @@ import {Grid} from '@/components/layout/grid';
 
 export const AnalysisStatsLayout = ({children}: React.PropsWithChildren) => {
   return (
-    <Grid center className="relative grid-cols-2 gap-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
+    <Grid center className="relative grid-cols-2 gap-1 md:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
       {children}
     </Grid>
   );
