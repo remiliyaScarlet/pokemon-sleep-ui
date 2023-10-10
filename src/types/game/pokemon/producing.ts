396497c3b6638f1b088aaa2c874a6d744b7607d1
RaenonX <raenonx0710@gmail.com>
2023-10-10 17:03:32 -0500

    Updated typing of Pokémon producing params

--- src/types/game/pokemon/producing.ts
+++ src/types/game/pokemon/producing.ts
@@ -6,9 +6,28 @@ export type PokemonProducingItem<TId> = {
   qty: number,
 };
 
+export type PokemonProducingParamsType = 'ingredient' | 'skill';
+
+export const pokemonProducingParamsConfidenceLevel = [
+  2, // Very Good
+  1, // Good
+  0, // Decent
+  -1, // Poor
+  -2, // Very Poor
+];
+
+export type PokemonProducingParamsConfidenceLevel = typeof pokemonProducingParamsConfidenceLevel[number];
+
+export type PokemonProducingParamsConfidence = {
+  [key in PokemonProducingParamsType]: PokemonProducingParamsConfidenceLevel
+};
+
 export type PokemonProducingParams = {
   pokemonId: PokemonId,
+  dataCount: number,
   ingredientSplit: number,
+  skillValue: number,
+  confidence: PokemonProducingParamsConfidence,
 };
 
 export type PokemonProducingParamsMap = {[pokemonId in PokemonId]?: PokemonProducingParams};
