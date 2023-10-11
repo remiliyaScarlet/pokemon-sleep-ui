d3005fa9cfc0d1e18d9fcbd6591eb05911b4fd7e
RaenonX <raenonx0710@gmail.com>
2023-10-10 22:36:04 -0500

    Added filter state updater interceptor in `useFilterInput()`

--- src/components/input/filter/hook.ts
+++ src/components/input/filter/hook.ts
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import {FilterInclusionMap} from '@/components/input/filter/type';
+import {ReactStateUpdaterFromOriginal} from '@/types/react';
 import {Indexable} from '@/utils/type';
 
 
@@ -10,6 +11,7 @@ type UseFilterInputOpts<TFilter, TData, TId extends Indexable> = {
   initialFilter: TFilter,
   isDataIncluded: (filter: TFilter, data: TData) => boolean,
   deps?: React.DependencyList,
+  onSetFilter?: (original: TFilter, updated: TFilter) => TFilter,
 };
 
 export const useFilterInput = <TFilter, TData, TId extends Indexable>({
@@ -18,13 +20,26 @@ export const useFilterInput = <TFilter, TData, TId extends Indexable>({
   initialFilter,
   isDataIncluded,
   deps,
+  onSetFilter,
 }: UseFilterInputOpts<TFilter, TData, TId>) => {
-  const [filter, setFilter] = React.useState<TFilter>(initialFilter);
+  const [filter, setFilterInternal] = React.useState<TFilter>(initialFilter);
   const isIncluded = React.useMemo((): FilterInclusionMap<TId> => (
     Object.fromEntries(data.map((single) => (
       [dataToId(single), isDataIncluded(filter, single)]
     ))) as FilterInclusionMap<TId>
   ), [filter, ...(deps ?? [])]);
+
+  const setFilter: ReactStateUpdaterFromOriginal<TFilter> = React.useCallback((getUpdated) => (
+    setFilterInternal((original) => {
+      const updated = getUpdated(original);
+
+      if (!onSetFilter) {
+        return updated;
+      }
+
+      return onSetFilter(original, updated);
+    })
+  ), [onSetFilter, setFilterInternal]);
 
   return {filter, setFilter, isIncluded};
 };
