16f53451385d625f1e98bf49c80b491ce9ae4d94
RaenonX <raenonx0710@gmail.com>
2023-10-19 14:28:05 -0500

    Updated activation email title

--- src/utils/user/activation/email.ts
+++ src/utils/user/activation/email.ts
@@ -11,7 +11,7 @@ export const sendUserActivationEmail = async ({recipient, activationLink}: SendU
     server: process.env.SUBSCRIPTION_EMAIL_SERVER,
     from: process.env.SUBSCRIPTION_EMAIL_FROM,
     to: recipient,
-    subject: 'Activation',
+    subject: 'Pokemon Sleep 訂閱啟用連結 / Subscription Activation Link',
     text: activationLink,
     html: activationLink,
   });
