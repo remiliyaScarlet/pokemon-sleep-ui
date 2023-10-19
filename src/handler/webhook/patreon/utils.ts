c755ceddd05c1586964bfba7f41e369e072cf387
RaenonX <raenonx0710@gmail.com>
2023-10-19 08:23:40 -0500

    Added utils for converting Patreon payload to activation properties #376

--- src/handler/webhook/patreon/utils.ts
+++ src/handler/webhook/patreon/utils.ts
@@ -1,5 +1,9 @@
 import crypto from 'crypto';
 
+import {UserActivationProperties} from '@/types/mongo/activation';
+import {PatreonWebhookPayload} from '@/types/patreon/webhook';
+import {getActivationExpiry} from '@/utils/user/activation/utils';
+
 
 type ThrowIfSignatureFailedOpts = {
   message: string,
@@ -21,4 +25,33 @@ export const throwIfSignatureFailed = ({message, expected}: ThrowIfSignatureFail
   }
 
   throw new Error(`Patreon signature mismatch / message: ${message}`);
+};
+
+export const toActivationProperties = (payload: PatreonWebhookPayload): UserActivationProperties | null => {
+  const {email, social_connections: social} = payload.included[1].attributes;
+  const {
+    access_expires_at: accessExpiry,
+    last_charge_status: chargeStatus,
+  } = payload.data.attributes;
+
+  if (chargeStatus !== 'Paid') {
+    return null;
+  }
+
+  return {
+    expiry: getActivationExpiry(accessExpiry),
+    activation: {
+      adsFree: true,
+      premium: true,
+    },
+    source: 'patreon',
+    contact: {
+      patreon: email,
+      ...(social.discord ? {
+        discord: `<@${social.discord.user_id}>`,
+      } : {}),
+    },
+    isSpecial: false,
+    note: '',
+  };
 };
