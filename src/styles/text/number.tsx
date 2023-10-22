9fb4e68398ebfe6b9dec6116c2dba3f96eb416cc
RaenonX <raenonx0710@gmail.com>
2023-10-22 04:13:41 -0500

    Fixed incorrect number style on hover

--- src/styles/text/number.tsx
+++ src/styles/text/number.tsx
@@ -9,7 +9,7 @@ export const getNumberStyles = ({num, base = 0}: GetNumberStylesOpts): string | 
   }
 
   if (num && num < base) {
-    return 'text-red-700 dark:text-red-300 group-hover:text-red-700 dark:group-hover:text-red-300';
+    return 'text-red-700 dark:text-red-300 group-hover:text-red-300 dark:group-hover:text-red-700';
   }
 
   return null;
