cca76a0768820d8750ebb7b0fecf78e2af4d8648
RaenonX <raenonx0710@gmail.com>
2023-10-27 09:51:55 -0500

    Fixed meal level bonus at level 1 #396

--- src/utils/game/meal/level.ts
+++ src/utils/game/meal/level.ts
@@ -1,3 +1,4 @@
 export const getMealLevelBonus = (level: number) => {
-  return 1 + (level * 0.02);
+  // Level 1 should have level bonus of 1x instead of 1.02x
+  return 1 + ((level - 1) * 0.02);
 };
