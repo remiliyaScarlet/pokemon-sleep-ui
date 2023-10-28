e6d44775f0289203c4ce0cdc6175a2376d552e77
RaenonX <raenonx0710@gmail.com>
2023-10-28 14:45:38 -0500

    Added cooking ingredient count utils

--- src/ui/cooking/utils.ts
+++ src/ui/cooking/utils.ts
@@ -1,6 +1,8 @@
 import {defaultCookingPreset} from '@/const/user/cooking';
+import {MealIngredient} from '@/types/game/meal/main';
 import {UserCookingPreset} from '@/types/userData/cooking';
-import {CookingFilter, CookingPreloadedData} from '@/ui/cooking/type';
+import {CookingFilter, CookingIngredientCount, CookingPreloadedData} from '@/ui/cooking/type';
+import {isNotNullish} from '@/utils/type';
 
 
 type ToCookingPresetOpts = {
@@ -20,3 +22,38 @@ export const toCookingPreset = ({preloaded, filter}: ToCookingPresetOpts): UserC
     showUnmakeableRecipe: filter.showUnmakeableRecipe,
   };
 };
+
+export const toMealIngredientFromCookingCount = (ingredientCount: CookingIngredientCount): MealIngredient[] => {
+  return Object.entries(ingredientCount)
+    .map(([id, quantity]) => {
+      if (!quantity) {
+        return null;
+      }
+
+      return {id: parseInt(id), quantity};
+    })
+    .filter(isNotNullish);
+};
+
+export const toCookingCountFromMealIngredient = (ingredients: MealIngredient[]): CookingIngredientCount => (
+  Object.fromEntries(ingredients.map(({id, quantity}) => [id, quantity]))
+);
+
+export const subtractIngredientCount = (
+  minuend: CookingIngredientCount,
+  subtrahend: CookingIngredientCount,
+): CookingIngredientCount => (
+  Object.fromEntries(Object.entries(minuend).map(([id, count]) => {
+    if (!count) {
+      return null;
+    }
+
+    const result = count - (subtrahend[parseInt(id)] ?? 0);
+
+    if (result < 0) {
+      return null;
+    }
+
+    return [id, result];
+  }).filter(isNotNullish))
+);
