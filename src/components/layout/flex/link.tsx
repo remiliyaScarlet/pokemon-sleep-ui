e7ab4f2b6ad9e1b26378bce92b848736576cb71f
RaenonX <raenonx0710@gmail.com>
2023-10-28 22:19:38 -0500

    Added `target` prop for `<FlexLink/>`

--- src/components/layout/flex/link.tsx
+++ src/components/layout/flex/link.tsx
@@ -1,6 +1,6 @@
 import {UrlObject} from 'url';
 
-import React from 'react';
+import React, {HTMLAttributeAnchorTarget} from 'react';
 
 import {Link} from '@/components/i18n';
 import {FlexCommonProps} from '@/components/layout/flex/type';
@@ -9,16 +9,18 @@ import {getFlexStyles} from '@/components/layout/flex/utils';
 
 type Props = FlexCommonProps & {
   href: string | UrlObject,
+  target?: HTMLAttributeAnchorTarget,
 };
 
 const FlexLinkInternal = ({
   direction = 'row',
   noFullWidth = true,
   href,
+  target,
   children,
   ...props
 }: React.PropsWithChildren<Props>, ref: React.ForwardedRef<HTMLAnchorElement>) => (
-  <Link ref={ref} href={href} className={getFlexStyles(direction, {noFullWidth, ...props})}>
+  <Link ref={ref} href={href} className={getFlexStyles(direction, {noFullWidth, ...props})} target={target}>
     {children}
   </Link>
 );
