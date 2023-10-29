5048184512c9e5e1bc5e9d310998926c15d37e12
RaenonX <raenonx0710@gmail.com>
2023-10-29 03:08:43 -0500

    Updated typing of `cloneMerge()`

--- src/utils/object/cloneMerge.ts
+++ src/utils/object/cloneMerge.ts
@@ -1,11 +1,11 @@
 import merge from 'lodash/merge';
 
-import {DeepPartial} from '@/utils/type';
+import {DeepPartial, Nullable} from '@/utils/type';
 
 
 export const cloneMerge = <TSource>(
   source: TSource,
-  ...others: (DeepPartial<TSource> | null | undefined)[]
+  ...others: Nullable<DeepPartial<TSource>>[]
 ): TSource => {
   // Have an empty object as the first so the merged object is cloned (and `source` won't be modified)
   // https://stackoverflow.com/a/28044419/11571888
