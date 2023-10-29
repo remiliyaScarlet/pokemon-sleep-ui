9a3987f5bb3dcd0015d5d40281f29c5f94753077
RaenonX <raenonx0710@gmail.com>
2023-10-29 10:08:05 -0500

    Removed unused DOM node in meal maker control

--- src/ui/cooking/make/input/control.tsx
+++ src/ui/cooking/make/input/control.tsx
@@ -20,35 +20,33 @@ export const MealMakerInputControl = ({filter, setFilter, preloaded}: MealMakerC
   const t = useTranslations('UI.InPage.Cooking');
 
   return (
-    <InputRow>
-      <Flex direction="row" className="justify-end gap-2">
-        <ToggleButton
-          id="makeableRecipe"
-          active={showUnmakeableRecipe}
-          onClick={() => setFilter((original) => ({
-            ...original,
-            showUnmakeableRecipe: !showUnmakeableRecipe,
-          }))}
-          className={getTextFilterButtonClass(showUnmakeableRecipe)}
-        >
-          <Flex direction="row" noFullWidth className="gap-1">
-            <div className="h-5 w-5">
-              {showUnmakeableRecipe ? <EyeIcon/> : <EyeSlashIcon/>}
-            </div>
-            <GenericIcon
-              src="/images/generic/ingredient_slash.png"
-              alt={t('ToggleUnmakeable')}
-              dimension="h-5 w-5"
-              noInvert
-              isActive={showUnmakeableRecipe}
-            />
-          </Flex>
-        </ToggleButton>
-        <UserDataUploadButton opts={{
-          type: 'cooking',
-          data: toCookingPreset({preloaded, filter}),
-        }}/>
-      </Flex>
+    <InputRow className="justify-end gap-2">
+      <ToggleButton
+        id="makeableRecipe"
+        active={showUnmakeableRecipe}
+        onClick={() => setFilter((original) => ({
+          ...original,
+          showUnmakeableRecipe: !showUnmakeableRecipe,
+        }))}
+        className={getTextFilterButtonClass(showUnmakeableRecipe)}
+      >
+        <Flex direction="row" noFullWidth className="gap-1">
+          <div className="h-5 w-5">
+            {showUnmakeableRecipe ? <EyeIcon/> : <EyeSlashIcon/>}
+          </div>
+          <GenericIcon
+            src="/images/generic/ingredient_slash.png"
+            alt={t('ToggleUnmakeable')}
+            dimension="h-5 w-5"
+            noInvert
+            isActive={showUnmakeableRecipe}
+          />
+        </Flex>
+      </ToggleButton>
+      <UserDataUploadButton opts={{
+        type: 'cooking',
+        data: toCookingPreset({preloaded, filter}),
+      }}/>
     </InputRow>
   );
 };
