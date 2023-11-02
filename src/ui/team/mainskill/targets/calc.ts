221f7790b5a31122830a40cdfefade049adb35ae
RaenonX <raenonx0710@gmail.com>
2023-11-01 23:03:27 -0500

    Fixed changing settings in skill trigger analysis does not apply immediately

--- src/ui/team/mainskill/targets/calc.ts
+++ src/ui/team/mainskill/targets/calc.ts
@@ -12,12 +12,15 @@ type UseSkillTriggerAnalysisCalculatedReturn = {
 export const useSkillTriggerAnalysisCalculated = (
   opts: GetSkillTriggerValueOfTargetsOpts,
 ): UseSkillTriggerAnalysisCalculatedReturn => {
-  const {state} = opts;
+  const {state, calculatedSettings} = opts;
 
   const [units, setUnits] = React.useState(getSkillTriggerValueOfTargets(opts));
 
   // Recalculate when `state` changes
-  React.useEffect(() => setUnits(getSkillTriggerValueOfTargets(opts)), [state]);
+  React.useEffect(
+    () => setUnits(getSkillTriggerValueOfTargets(opts)),
+    [state, calculatedSettings],
+  );
 
   const sort = React.useCallback(
     () => setUnits((original) => [
@@ -26,7 +29,7 @@ export const useSkillTriggerAnalysisCalculated = (
         b.skillTriggerValue.actual - a.skillTriggerValue.actual
       )),
     ]),
-    [units, state],
+    [units, state, calculatedSettings],
   );
 
   return {units, sort};
