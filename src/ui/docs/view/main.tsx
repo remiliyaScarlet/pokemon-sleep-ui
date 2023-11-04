6927a77127a5c3282571bfa0e8afcb97f0e47bbc
RaenonX <raenonx0710@gmail.com>
2023-11-04 18:01:31 -0500

    Added ads in docs view #360

--- src/ui/docs/view/main.tsx
+++ src/ui/docs/view/main.tsx
@@ -3,11 +3,13 @@ import React from 'react';
 import {notFound} from 'next/navigation';
 import {getServerSession} from 'next-auth';
 
+import {AdsUnit} from '@/components/ads/main';
 import {DocsControl} from '@/components/shared/docs/control/main';
 import {DocsMeta} from '@/components/shared/docs/meta/main';
 import {DocsContentView} from '@/components/shared/docs/view/main';
 import {authOptions} from '@/const/auth';
 import {getDocBySlug} from '@/controller/docs';
+import {isCmsMod} from '@/controller/user/account/common';
 import {PageProps} from '@/types/next/page/common';
 import {DocsPageParams} from '@/types/next/page/docs';
 import {PublicPageLayout} from '@/ui/base/layout/public';
@@ -33,9 +35,12 @@ export const DocsView = async ({params}: PageProps<DocsPageParams>) => {
 
   return (
     <PublicPageLayout locale={locale}>
+      <AdsUnit/>
       <DocsMeta doc={doc}/>
       <DocsControl session={session} path={doc.path}/>
+      <AdsUnit/>
       <DocsContentView locale={locale} doc={doc}/>
+      <AdsUnit/>
     </PublicPageLayout>
   );
 };
