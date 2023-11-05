17cad3ca689cfb6b28f7eac38049d269fcf18c7a
RaenonX <raenonx0710@gmail.com>
2023-11-05 14:29:33 -0600

    Refactored Ads popup text

--- src/components/ads/popup/main.tsx
+++ src/components/ads/popup/main.tsx
@@ -1,12 +1,9 @@
 'use client';
 import React from 'react';
 
-import {useTranslations} from 'next-intl';
-import ReactMarkdown from 'react-markdown';
-import remarkGfm from 'remark-gfm';
-
 import {AdsConfiguredDisplay} from '@/components/ads/configured/display';
 import {useAdsPopup} from '@/components/ads/popup/hook';
+import {AdsPopupText} from '@/components/ads/popup/text';
 import {Flex} from '@/components/layout/flex/common';
 import {PopupUltimate} from '@/components/popup/ultimate/main';
 
@@ -15,14 +12,10 @@ export const AdsPopup = () => {
   const {state, setShow} = useAdsPopup();
   const {show, locked} = state;
 
-  const t = useTranslations('UI.Ads');
-
   return (
     <PopupUltimate show={show} setShow={setShow} closeDisabled={locked}>
       <Flex className="h-[70vh] gap-1.5 md:w-[60vw] lg:w-[50vw]">
-        <ReactMarkdown remarkPlugins={[remarkGfm]} className="markdown text-xl">
-          {t('Popup')}
-        </ReactMarkdown>
+        <AdsPopupText/>
         <AdsConfiguredDisplay alwaysSingle/>
         <AdsConfiguredDisplay alwaysSingle/>
       </Flex>
---
+++ src/components/ads/popup/text.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import {useTranslations} from 'next-intl';
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+
+
+export const AdsPopupText = () => {
+  const t = useTranslations('UI.Ads');
+
+  return (
+    <ReactMarkdown remarkPlugins={[remarkGfm]} className="markdown text-xl">
+      {t('Popup')}
+    </ReactMarkdown>
+  );
+};
