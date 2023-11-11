cb3c67c2134f9144a3aebf73518b7652a8886b4b
RaenonX <raenonx0710@gmail.com>
2023-11-11 07:09:14 -0600

    Removed unused `@ts-ignore`

--- src/components/ocr/hook.ts
+++ src/components/ocr/hook.ts
@@ -79,8 +79,6 @@ export const useOcr = ({
     await worker.setParameters({
       // 'S' could be mistakenly recognized as `$` in JP
       // 'S' could be mistakenly recognized as `§` in EN
-      // https://github.com/naptha/tesseract.js/issues/831
-      // @ts-ignore
       tessedit_char_blacklist: '$§',
       tessedit_pageseg_mode: PSM.SPARSE_TEXT,
     });
