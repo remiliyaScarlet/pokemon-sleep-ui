9b85882b2363a0c4d13159cf53270bece83e9519
RaenonX <raenonx0710@gmail.com>
2023-11-14 00:00:34 -0600

    Updated ads height

--- src/components/ads/const.ts
+++ src/components/ads/const.ts
@@ -1,6 +1,6 @@
 export const adsClientId = 'ca-pub-1535004092052078';
 
-export const adsHeight = 'h-48 lg:h-32';
+export const adsHeight = 'h-44 lg:h-28';
 
 export const adsHeightAdBlockActive = 'min-h-[15rem] lg:min-h-[12rem]';
 
