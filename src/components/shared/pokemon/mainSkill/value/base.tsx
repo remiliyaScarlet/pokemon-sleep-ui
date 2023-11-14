8f04b5dcf0ac48edf4c8d85fca8be9bc370f343a
RaenonX <raenonx0710@gmail.com>
2023-11-13 21:37:03 -0600

    Added `dimension` prop for various icons

--- src/components/shared/pokemon/mainSkill/value/base.tsx
+++ src/components/shared/pokemon/mainSkill/value/base.tsx
@@ -4,19 +4,21 @@ import {useTranslations} from 'next-intl';
 
 import {Flex} from '@/components/layout/flex/common';
 import {GenericMainSkillIcon} from '@/components/shared/pokemon/mainSkill/icon/generic';
+import {Dimension} from '@/types/style';
 import {formatFloat3} from '@/utils/number/format';
 
 
 type Props = {
   value: number,
+  dimension?: Dimension,
 };
 
-export const PokemonMainSkillValue = ({value}: Props) => {
+export const PokemonMainSkillValue = ({value, dimension}: Props) => {
   const t = useTranslations('UI.InPage.Pokedex.Stats');
 
   return (
     <Flex direction="row" noFullWidth className="items-center gap-1">
-      <GenericMainSkillIcon alt={t('MainSkillValue')}/>
+      <GenericMainSkillIcon alt={t('MainSkillValue')} dimension={dimension}/>
       <div>{formatFloat3(value)}</div>
     </Flex>
   );
--- src/components/shared/pokemon/production/ingredientRate.tsx
+++ src/components/shared/pokemon/production/ingredientRate.tsx
@@ -4,20 +4,22 @@ import {useTranslations} from 'next-intl';
 
 import {Flex} from '@/components/layout/flex/common';
 import {GenericIngredientIcon} from '@/components/shared/icon/ingredient';
+import {Dimension} from '@/types/style';
 import {formatFloat, formatFloat3} from '@/utils/number/format';
 
 
 type Props = {
   split: number,
   noIcon?: boolean,
+  dimension?: Dimension,
 };
 
-export const PokemonIngredientRate = ({split, noIcon}: Props) => {
+export const PokemonIngredientRate = ({split, noIcon, dimension}: Props) => {
   const t = useTranslations('UI.InPage.Pokedex.Sort');
 
   return (
     <Flex direction="row" noFullWidth className="items-center gap-1">
-      {!noIcon && <GenericIngredientIcon alt={t('IngredientRate')}/>}
+      {!noIcon && <GenericIngredientIcon alt={t('IngredientRate')} dimension={dimension}/>}
       <span>
         {formatFloat(split * 100)}% (<span className="text-xs">1:</span>{formatFloat3(1 / split)})
       </span>
