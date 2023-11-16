f9d693959e29efaedc9558efec2c5bbef447dece
RaenonX <raenonx0710@gmail.com>
2023-11-16 00:21:01 -0600

    Added `<PremiumOnly/>`

--- src/hooks/toast/main.tsx
+++ src/hooks/toast/main.tsx
@@ -1,21 +1,14 @@
 import React from 'react';
 
-import {useTranslations} from 'next-intl';
-import ReactMarkdown from 'react-markdown';
-import remarkGfm from 'remark-gfm';
-
+import {PremiumOnly} from '@/components/static/premiumOnly';
 import {showToast} from '@/utils/toast';
 
 
 export const usePremiumRequiredToast = () => {
-  const t = useTranslations('UI.Subscription');
-
   const showPremiumRequiredToast = React.useCallback(() => showToast({
     isAlert: true,
     content: (
-      <ReactMarkdown remarkPlugins={[remarkGfm]} className="[&_a]:text-link-inverse">
-        {t('PremiumOnly')}
-      </ReactMarkdown>
+      <PremiumOnly className="[&_a]:text-link-inverse"/>
     ),
   }), []);
 
