19dc18727a9dcdba23484b091ed77475aef8c16d
RaenonX <raenonx0710@gmail.com>
2023-11-17 15:14:52 -0600

    Added `shrink-0` to `<PremiumIcon/>`

--- src/components/static/premium/icon.tsx
+++ src/components/static/premium/icon.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import LockClosedIcon from '@heroicons/react/24/solid/LockClosedIcon';
+import {clsx} from 'clsx';
 
 import {Dimension} from '@/types/style';
 
@@ -10,5 +11,5 @@ type Props = {
 };
 
 export const PremiumIcon = ({dimension}: Props) => {
-  return <LockClosedIcon className={dimension ?? 'h-5 w-5'}/>;
+  return <LockClosedIcon className={clsx('shrink-0', dimension ?? 'h-5 w-5')}/>;
 };
