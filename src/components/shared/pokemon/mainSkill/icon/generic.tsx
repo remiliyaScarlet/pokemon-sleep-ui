7d35740bbd4ecaf2a45aae04e90f11583fedc06b
RaenonX <raenonx0710@gmail.com>
2023-11-18 04:41:19 -0600

    Updated props of `<GenericMainSkillIcon/>`

--- src/components/shared/pokemon/mainSkill/icon/generic.tsx
+++ src/components/shared/pokemon/mainSkill/icon/generic.tsx
@@ -1,20 +1,9 @@
 import React from 'react';
 
 import {GenericIcon} from '@/components/shared/icon/common/main';
-import {Dimension} from '@/types/style';
+import {IconProps} from '@/components/shared/icon/type';
 
 
-type Props = {
-  alt: string,
-  dimension?: Dimension,
-};
-
-export const GenericMainSkillIcon = ({alt, dimension}: Props) => {
-  return (
-    <GenericIcon
-      src="/images/generic/mainSkill.png"
-      alt={alt}
-      dimension={dimension}
-    />
-  );
+export const GenericMainSkillIcon = (props: IconProps) => {
+  return <GenericIcon src="/images/generic/mainSkill.png" {...props}/>;
 };
