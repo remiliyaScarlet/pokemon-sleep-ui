77bdc8387452eeae0f870331ed2ca74aeebea45d
RaenonX <raenonx0710@gmail.com>
2023-11-22 01:55:35 -0600

    Refactored filter category input

--- src/components/input/filter/category.tsx
+++ src/components/input/filter/category.tsx
@@ -1,57 +1,10 @@
 import React from 'react';
 
-import {clsx} from 'clsx';
-
-import {InputRowWithTitle} from '@/components/input/filter/rowWithTitle';
+import {FilterSingleCategoryInput} from '@/components/input/filter/base/single';
 import {FilterCategoryInputProps} from '@/components/input/filter/type';
-import {ToggleButton} from '@/components/input/toggleButton';
-import {Flex} from '@/components/layout/flex/common';
 import {IndexableNonSymbol} from '@/utils/type';
 
 
-export const FilterCategoryInput = <TId extends IndexableNonSymbol | null>({
-  title,
-  ids,
-  idToButton,
-  isHidden,
-  onClick,
-  isActive,
-  classNameOfButton,
-  noWrap,
-  ...rowOpts
-}: FilterCategoryInputProps<TId>) => {
-  const {noFixedTitleWidth} = rowOpts;
-
-  return (
-    <InputRowWithTitle
-      title={
-        <div className={clsx('whitespace-nowrap text-center text-sm', !noFixedTitleWidth && 'w-32')}>
-          {title}
-        </div>
-      }
-      {...rowOpts}
-    >
-      <Flex direction="row" center wrap={!noWrap} className="gap-1 sm:justify-normal">
-        {ids.map((id) => {
-          const active = isActive(id);
-          const hidden = isHidden && isHidden(id);
-
-          if (hidden) {
-            return null;
-          }
-
-          return (
-            <ToggleButton
-              key={id}
-              active={active}
-              onClick={() => onClick(id)}
-              className={classNameOfButton}
-            >
-              {idToButton(id, active)}
-            </ToggleButton>
-          );
-        })}
-      </Flex>
-    </InputRowWithTitle>
-  );
+export const FilterCategoryInput = <TId extends IndexableNonSymbol | null>(props: FilterCategoryInputProps<TId>) => {
+  return <FilterSingleCategoryInput {...props}/>;
 };
