861ec9d4f4d3e991d003c8d09ffbd6caae621ec9
RaenonX <raenonx0710@gmail.com>
2023-11-24 02:04:16 -0600

    Updated default target meal

--- src/const/user/cooking.ts
+++ src/const/user/cooking.ts
@@ -4,7 +4,23 @@ import {NonNullableRecord} from '@/utils/type';
 
 export const defaultCookingPreset: NonNullableRecord<UserCookingPreset> = {
   mealType: 1,
-  target: {},
+  target: {
+    1: {
+      breakfast: 1005,
+      lunch: 1005,
+      dinner: 1005,
+    },
+    2: {
+      breakfast: 2011,
+      lunch: 2011,
+      dinner: 2011,
+    },
+    3: {
+      breakfast: 3007,
+      lunch: 3007,
+      dinner: 3007,
+    },
+  },
   potCapacity: 15,
   ingredients: {},
   showEnergy: true,
