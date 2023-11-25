4c4659256aa311f52a3ce1094d72cad773f33fe2
RaenonX <raenonx0710@gmail.com>
2023-11-24 19:23:26 -0600

    Added account info visibility toggle

--- src/ui/base/navbar/userSettings/sections/account.tsx
+++ src/ui/base/navbar/userSettings/sections/account.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 
+import EyeIcon from '@heroicons/react/24/solid/EyeIcon';
+import EyeSlashIcon from '@heroicons/react/24/solid/EyeSlashIcon';
 import {Session} from 'next-auth';
 
+import {AnimatedCollapseQuick} from '@/components/layout/collapsible/animatedQuick';
 import {Flex} from '@/components/layout/flex/common';
 
 
@@ -10,9 +13,17 @@ type Props = {
 };
 
 export const UserSettingsAccountInfo = ({session}: Props) => {
+  const [show, setShow] = React.useState(false);
+
   return (
-    <Flex className="rounded-lg border border-slate-500 p-2">
-      {session.user.email}
+    <Flex className="rounded-lg p-2 shadow-border shadow-slate-500 md:flex-row">
+      <button className="button-clickable-bg h-8 w-8 self-center p-1" onClick={() => setShow(!show)}>
+        {show ? <EyeSlashIcon/> : <EyeIcon/>}
+      </button>
+      <AnimatedCollapseQuick show={show}>
+        <div>{session.user.email}</div>
+        <code className="text-fuchsia-700 dark:text-fuchsia-400">{session.user.id}</code>
+      </AnimatedCollapseQuick>
     </Flex>
   );
 };
