a013795fcf12b4df25cb3c942b5866ef3f9cc1e5
RaenonX <raenonx0710@gmail.com>
2023-12-04 04:20:56 -0600

    Refactored rating result UI

--- src/components/shared/pokemon/rating/single.tsx
+++ src/components/shared/pokemon/rating/single.tsx
@@ -3,24 +3,17 @@ import React from 'react';
 import BeakerIcon from '@heroicons/react/24/outline/BeakerIcon';
 import HandThumbDownIcon from '@heroicons/react/24/outline/HandThumbDownIcon';
 import HandThumbUpIcon from '@heroicons/react/24/outline/HandThumbUpIcon';
-import {clsx} from 'clsx';
 
 import {CollapsibleFull} from '@/components/layout/collapsible/full';
 import {useCollapsible} from '@/components/layout/collapsible/hook';
 import {Flex} from '@/components/layout/flex/common';
 import {Grid} from '@/components/layout/grid';
 import {LazyLoad} from '@/components/layout/lazyLoad';
-import {ProgressBar} from '@/components/progressBar';
-import {LevelIcon} from '@/components/shared/icon/lv';
-import {ratingMarkThresholdByPr} from '@/components/shared/pokemon/rating/const';
 import {RatingDataPointUI} from '@/components/shared/pokemon/rating/point';
-import {PokemonRatingRelativeStrength} from '@/components/shared/pokemon/rating/relativeStrength';
+import {RatingResultUI} from '@/components/shared/pokemon/rating/result';
 import {RatingResultProps} from '@/components/shared/pokemon/rating/type';
 import {useRatingWorker} from '@/hooks/rating/hook';
-import {classOfMarkStyle} from '@/styles/text/mark/style';
-import {getMarkByThreshold} from '@/styles/text/mark/utils';
 import {PokemonKeyLevel} from '@/types/game/pokemon/level';
-import {formatFloat, formatInt, formatToAbbreviation} from '@/utils/number/format';
 
 
 type Props = Omit<RatingResultProps, 'pokemonMaxLevel'> & {
@@ -68,55 +61,12 @@ export const RatingResultOfLevelUI = ({
     rate(request.setup);
   }, [request?.timestamp]);
 
-  const {
-    samples,
-    rank,
-    percentage,
-    percentile,
-    baseDiffPercent,
-    points,
-  } = result;
-  const textMarkStyle = getMarkByThreshold(percentile, ratingMarkThresholdByPr);
+  const {points} = result;
   const basis = request?.setup.basis;
 
   return (
     <LazyLoad loading={loading} loadingFullHeight className="info-section relative">
-      <Flex direction="row" className={clsx('gap-0.5', textMarkStyle && classOfMarkStyle[textMarkStyle])}>
-        <Flex center noFullWidth>
-          <LevelIcon dimension="h-10 w-10"/>
-          <div className="text-xl">{level}</div>
-        </Flex>
-        <Flex className="gap-3">
-          <Flex direction="row" className="items-end justify-center gap-1.5">
-            <Flex direction="row" className="items-end justify-center gap-1 text-6xl">
-              {isNaN(percentile) ? '-' : <><span className="text-2xl">PR</span>{formatInt(percentile)}</>}
-            </Flex>
-            <PokemonRatingRelativeStrength
-              baseDiffPercent={baseDiffPercent}
-              iconDimension="h-7 w-7"
-              className="text-2xl"
-            />
-          </Flex>
-          <Flex>
-            <ProgressBar percent={percentile}/>
-          </Flex>
-          <Flex direction="row" className="items-end justify-center gap-1.5">
-            <Flex direction="row" className="items-end justify-center gap-1">
-              <div className="text-2xl">{rank ? formatInt(rank) : '-'}</div>
-              <div>/</div>
-              <div className="whitespace-nowrap">
-                {isNaN(samples) ? '-' : formatToAbbreviation({num: samples})}
-              </div>
-            </Flex>
-            <div className="text-xl">
-              {isNaN(percentage) ? '-' : `${formatFloat(percentage)}%`}
-            </div>
-          </Flex>
-          <Flex>
-            <ProgressBar percent={percentage}/>
-          </Flex>
-        </Flex>
-      </Flex>
+      <RatingResultUI result={result} level={level}/>
       <CollapsibleFull state={collapsible} disabled={!points.min && !points.current && !points.max} button={
         <Flex direction="row" center className="gap-1">
           <HandThumbDownIcon className="h-6 w-6"/>
