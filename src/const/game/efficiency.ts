78d757ab3cb99667275ac4736f6793485589a241
RaenonX <raenonx0710@gmail.com>
2023-12-05 17:29:25 -0600

    Updated stamina efficiency multipliers

--- src/const/game/efficiency.ts
+++ src/const/game/efficiency.ts
@@ -3,9 +3,9 @@ import {EfficiencyBreakPoint} from '@/types/game/producing/efficiency';
 
 export const efficiencyBeforeBreakPoint: {[breakPoint in EfficiencyBreakPoint]: number} = {
   80: 1 / 0.45,
-  60: 1.92,
-  40: 1.61,
-  20: 1.41,
+  60: 1 / 0.52,
+  40: 1 / 0.62,
+  20: 1 / 0.71,
   0: 1,
 };
 
--- src/utils/game/stamina/efficiency.test.ts
+++ src/utils/game/stamina/efficiency.test.ts
@@ -7,12 +7,12 @@ describe('Efficiency at certain stamina', () => {
   it('has correct efficiency', () => {
     expect(getEfficiency({stamina: 100})).toBeCloseTo(1 / 0.45);
     expect(getEfficiency({stamina: 90})).toBeCloseTo(1 / 0.45);
-    expect(getEfficiency({stamina: 80})).toBe(1.92);
-    expect(getEfficiency({stamina: 70})).toBe(1.92);
-    expect(getEfficiency({stamina: 60})).toBeCloseTo(1.61);
-    expect(getEfficiency({stamina: 50})).toBeCloseTo(1.61);
-    expect(getEfficiency({stamina: 40})).toBeCloseTo(1.41);
-    expect(getEfficiency({stamina: 30})).toBeCloseTo(1.41);
+    expect(getEfficiency({stamina: 80})).toBeCloseTo(1 / 0.52);
+    expect(getEfficiency({stamina: 70})).toBeCloseTo(1 / 0.52);
+    expect(getEfficiency({stamina: 60})).toBeCloseTo(1 / 0.62);
+    expect(getEfficiency({stamina: 50})).toBeCloseTo(1 / 0.62);
+    expect(getEfficiency({stamina: 40})).toBeCloseTo(1 / 0.71);
+    expect(getEfficiency({stamina: 30})).toBeCloseTo(1 / 0.71);
     expect(getEfficiency({stamina: 20})).toBe(1);
     expect(getEfficiency({stamina: 10})).toBe(1);
   });
