3757c75d58c5f5e72e5e9892aafd4749395a611f
RaenonX <raenonx0710@gmail.com>
2023-12-05 01:53:53 -0600

    Updated 81+ stamina efficiency to `1 / 0.45`

--- src/const/game/efficiency.ts
+++ src/const/game/efficiency.ts
@@ -2,7 +2,7 @@ import {EfficiencyBreakPoint} from '@/types/game/producing/efficiency';
 
 
 export const efficiencyBeforeBreakPoint: {[breakPoint in EfficiencyBreakPoint]: number} = {
-  80: 2.22,
+  80: 1 / 0.45,
   60: 1.92,
   40: 1.61,
   20: 1.41,
--- src/utils/game/stamina/efficiency.test.ts
+++ src/utils/game/stamina/efficiency.test.ts
@@ -5,8 +5,8 @@ import {getEfficiency} from '@/utils/game/stamina/efficiency';
 
 describe('Efficiency at certain stamina', () => {
   it('has correct efficiency', () => {
-    expect(getEfficiency({stamina: 100})).toBe(2.22);
-    expect(getEfficiency({stamina: 90})).toBe(2.22);
+    expect(getEfficiency({stamina: 100})).toBeCloseTo(1 / 0.45);
+    expect(getEfficiency({stamina: 90})).toBeCloseTo(1 / 0.45);
     expect(getEfficiency({stamina: 80})).toBe(1.92);
     expect(getEfficiency({stamina: 70})).toBe(1.92);
     expect(getEfficiency({stamina: 60})).toBeCloseTo(1.61);
