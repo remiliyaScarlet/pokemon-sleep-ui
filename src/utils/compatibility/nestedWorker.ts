89d2ecd49f702af736c7063d39d2edfeb4b40337
RaenonX <raenonx0710@gmail.com>
2023-12-06 12:49:40 -0600

    Updated nested worker check impl #548
    closes #548

--- src/utils/compatibility/nestedWorker.ts
+++ src/utils/compatibility/nestedWorker.ts
@@ -1,23 +1,25 @@
 export const isNestedWorkerSupported = (): boolean => {
   let supports = false;
 
-  const testerNested = {
-    get name() {
-      supports = true;
-      return 'nested';
-    },
-  };
-
-  const testerParent = {
-    get name() {
-      new Worker('data:,', testerNested).terminate();
-      return 'parent';
-    },
-  };
-
   try {
+    const testerNested = {
+      get name() {
+        supports = true;
+        return 'nested';
+      },
+    };
+
+    const testerParent = {
+      get name() {
+        new Worker('data:,', testerNested).terminate();
+        return 'parent';
+      },
+    };
+
     new Worker('data:,', testerParent).terminate();
-  } finally {}
+  } catch (e) {
+    return false;
+  }
 
   return supports;
 };
