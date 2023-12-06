002e7869d78d9ff7278aa5c45a51d091d576726e
RaenonX <raenonx0710@gmail.com>
2023-12-06 03:49:24 -0600

    Updated `useUserSettings()` to always run migration

--- src/hooks/userData/settings.tsx
+++ src/hooks/userData/settings.tsx
@@ -1,14 +1,22 @@
 import isEqual from 'lodash/isEqual';
 import {useCustomCompareMemo} from 'use-custom-compare';
 
+import {defaultUserSettings} from '@/const/user/settings';
 import {UseUserDataOpts} from '@/hooks/userData/type';
 import {UserSettings} from '@/types/userData/settings';
+import {migrate} from '@/utils/migrate/main';
+import {userSettingsMigrators} from '@/utils/migrate/userSettings/migrators';
 import {cloneMerge} from '@/utils/object/cloneMerge';
 
 
 export const useUserSettings = ({server, client}: UseUserDataOpts<UserSettings>): UserSettings => {
   return useCustomCompareMemo(
-    () => cloneMerge(server, client),
+    () => migrate({
+      original: defaultUserSettings,
+      override: cloneMerge(server, client),
+      migrators: userSettingsMigrators,
+      migrateParams: {},
+    }),
     [client],
     (prev, next) => isEqual(prev, next),
   );
