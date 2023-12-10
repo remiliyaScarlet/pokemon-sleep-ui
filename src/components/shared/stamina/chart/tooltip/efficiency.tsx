4f3bf6666ed67b3adda14864bd3e9ae4cbeb6822
RaenonX <raenonx0710@gmail.com>
2023-12-10 05:08:33 -0600

    Added efficiency floating number formatting in energy analysis

--- src/components/shared/stamina/chart/tooltip/efficiency.tsx
+++ src/components/shared/stamina/chart/tooltip/efficiency.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 
 import {StaminaChartTooltip} from '@/components/shared/stamina/chart/tooltip/common';
 import {StaminaChartTooltipCommonProps} from '@/components/shared/stamina/chart/tooltip/type';
+import {formatFloat3} from '@/utils/number/format';
 
 
 export const StaminaChartTooltipOfEfficiency = (props: StaminaChartTooltipCommonProps) => {
   return (
     <StaminaChartTooltip
       {...props}
-      getInfo={(efficiency) => `${efficiency}x`}
+      getInfo={(efficiency) => `${formatFloat3(efficiency)}x`}
     />
   );
 };
