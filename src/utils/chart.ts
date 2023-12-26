ef3c1412075be4b7c2c76a64048f511b2c8ec277
RaenonX <raenonx0710@gmail.com>
2023-12-26 16:43:43 -0600

    Added `generateTicksFromData()`

--- src/utils/chart.ts
+++ src/utils/chart.ts
@@ -15,3 +15,25 @@ export const generateTicks = ({max, interval, start}: GenerateTicksOpts): number
 
   return ticks;
 };
+
+type GenerateTicksFromDataOpts = {
+  data: number[],
+  count: number,
+};
+
+export const generateTicksFromData = ({data, count}: GenerateTicksFromDataOpts): number[] => {
+  const min = Math.min(...data);
+  const max = Math.max(...data);
+  const interval = (max - min) / (count - 1);
+
+  const ticks: number[] = [min];
+
+  let current = min;
+
+  while (Math.abs(current - max) > 1E-6) {
+    current += interval;
+    ticks.push(current);
+  }
+
+  return ticks;
+};
