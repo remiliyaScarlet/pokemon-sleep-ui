b9ef864dbef3b901b6a3dc92009b4e8a340da633
RaenonX <raenonx0710@gmail.com>
2023-12-31 03:48:56 -0600

    Updated `next-auth` API endpoint to use `edge` runtime

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,11 @@
+import {ServerRuntime} from 'next';
 import nextAuth from 'next-auth';
 
 import {authOptions} from '@/const/auth';
 
 
 const handler = nextAuth(authOptions);
+
+export const runtime: ServerRuntime = 'edge';
 
 export {handler as GET, handler as POST};
