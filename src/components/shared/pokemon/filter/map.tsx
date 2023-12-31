3faf038719bbe029e66af12755319d8bb8ba3d9a
RaenonX <raenonx0710@gmail.com>
2023-12-30 20:46:15 -0600

    Updated map filter to accept `premiumOnly`

--- src/components/shared/pokemon/filter/map.tsx
+++ src/components/shared/pokemon/filter/map.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
+import {Session} from 'next-auth';
 import {useTranslations} from 'next-intl';
 
 import {FilterInputOnClickProps} from '@/components/input/filter/common/type';
 import {FilterExpandedInput} from '@/components/input/filter/expanded/main';
 import {Flex} from '@/components/layout/flex/common';
 import {NextImage} from '@/components/shared/common/image/main';
+import {PremiumIcon} from '@/components/static/premium/icon';
+import {usePremiumRequiredToast} from '@/hooks/toast/main';
+import {useUserActivation} from '@/hooks/userData/activation';
 import {imageGallerySizes} from '@/styles/image';
 import {textFilterButtonStyle} from '@/styles/input';
 import {SleepMapId} from '@/types/game/sleepStyle';
@@ -15,11 +19,20 @@ type Props = FilterInputOnClickProps<SleepMapId> & {
   mapIds: SleepMapId[],
   highlight?: boolean,
   title?: React.ReactNode,
-};
+} & ({
+  premiumOnly: true,
+  session: Session | null,
+} | {
+  premiumOnly?: false,
+  session?: never,
+});
 
-export const PokemonMapFilter = ({mapIds, highlight, title, ...props}: Props) => {
+export const PokemonMapFilter = ({onClick, mapIds, highlight, title, premiumOnly, session, ...props}: Props) => {
   const t = useTranslations('UI.Common');
   const t2 = useTranslations('Game');
+
+  const {isPremium} = useUserActivation(session);
+  const {showPremiumRequiredToast} = usePremiumRequiredToast();
 
   return (
     <FilterExpandedInput
@@ -38,12 +51,21 @@ export const PokemonMapFilter = ({mapIds, highlight, title, ...props}: Props) =>
               />
             </div>
             <Flex direction="row" center className="absolute z-10 h-full">
-              {mapName}
+              {!isPremium && <PremiumIcon/>}
+              <div>{mapName}</div>
             </Flex>
           </>
         );
       }}
       className={textFilterButtonStyle}
+      onClick={(id) => {
+        if (!isPremium) {
+          showPremiumRequiredToast();
+          return;
+        }
+
+        onClick(id);
+      }}
       {...props}
     />
   );
