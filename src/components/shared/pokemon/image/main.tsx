0788f936d72310971d623da95a33b8598e3a35dd
RaenonX <raenonx0710@gmail.com>
2023-12-31 01:59:23 -0600

    Updated `<PokemonImage/>` to accept `className`

--- src/components/shared/pokemon/image/main.tsx
+++ src/components/shared/pokemon/image/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import {NextImage} from '@/components/shared/common/image/main';
+import {NextImage, NextImageProps} from '@/components/shared/common/image/main';
 import {PokemonImageType} from '@/components/shared/pokemon/image/type';
 import {imagePortraitSizes} from '@/styles/image';
 import {PokemonId} from '@/types/game/pokemon';
@@ -11,12 +11,22 @@ type Props = {
   image: PokemonImageType,
   isShiny: boolean,
   alt?: string,
+  className?: string,
 };
 
-export const PokemonImage = ({pokemonId, image, isShiny, alt}: Props) => {
+export const PokemonImage = ({pokemonId, image, isShiny, alt, className}: Props) => {
+  const commonImageProps: Pick<NextImageProps, 'sizes' | 'className'> = {
+    sizes: imagePortraitSizes,
+    className,
+  };
+
   if (!pokemonId) {
     return (
-      <NextImage src="/images/generic/pokeballUnavailable.png" alt="N/A" sizes={imagePortraitSizes}/>
+      <NextImage
+        {...commonImageProps}
+        src="/images/generic/pokeballUnavailable.png"
+        alt="N/A"
+      />
     );
   }
 
@@ -25,9 +35,9 @@ export const PokemonImage = ({pokemonId, image, isShiny, alt}: Props) => {
   if (image === 'portrait') {
     return (
       <NextImage
+        {...commonImageProps}
         src={`/images/pokemon/portrait/${isShiny ? 'shiny/' : ''}${pokemonId}.png`}
         alt={actualAlt}
-        sizes={imagePortraitSizes}
       />
     );
   }
@@ -35,18 +45,18 @@ export const PokemonImage = ({pokemonId, image, isShiny, alt}: Props) => {
   if (image === 'icon') {
     return (
       <NextImage
+        {...commonImageProps}
         src={`/images/pokemon/icons/${pokemonId}.png`}
         alt={actualAlt}
-        sizes={imagePortraitSizes}
       />
     );
   }
 
   return (
     <NextImage
+      {...commonImageProps}
       src={`/images/sleep/${image}/${isShiny ? 'shiny/' : ''}${pokemonId}.png`}
       alt={actualAlt}
-      sizes={imagePortraitSizes}
     />
   );
 };
