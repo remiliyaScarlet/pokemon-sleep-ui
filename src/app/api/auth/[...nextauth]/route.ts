2ddb67116a88086e37f70e02ee3a70e5e86b82cb
RaenonX <raenonx0710@gmail.com>
2023-12-31 03:53:35 -0600

    Bumped dependencies

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,11 +1,8 @@
-import {ServerRuntime} from 'next';
 import nextAuth from 'next-auth';
 
 import {authOptions} from '@/const/auth';
 
 
 const handler = nextAuth(authOptions);
-
-export const runtime: ServerRuntime = 'edge';
 
 export {handler as GET, handler as POST};
